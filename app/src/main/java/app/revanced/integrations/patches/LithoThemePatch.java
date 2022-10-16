5925a624233eda813c8fc3cd1d8d7492edf50cfc
inotia00 <ars258000@gmail.com>
2022-10-16 22:05:23 +0900

    improve amoled theme

--- app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
+++ app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
@@ -5,12 +5,11 @@ import app.revanced.integrations.utils.ThemeHelper;
 public class LithoThemePatch {
     //Used by app.revanced.patches.youtube.layout.theme.patch.LithoThemePatch
     public static int applyLithoTheme(int originalValue) {
-        int newValue = 0;
+        int[] ColorList = {-14606047, -15198184, -15790321, -98492127};
 
-        if (!ThemeHelper.isDarkTheme() && originalValue == -1) {
-            return newValue;
-        } else if (originalValue == -14606047 || originalValue == -15790321 || originalValue == -98492127) {
-            return newValue;
+        if (!ThemeHelper.isDarkTheme() && originalValue == -1) return 0;
+        for (int i = 0; i < ColorList.length ; i++) {
+            if (ColorList[i] == originalValue) return 0;
         }
 
         return originalValue;
