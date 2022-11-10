7a96c92673d2f5488bea50cb8a9ead2cf05bbdeb
inotia00 <ars258000@gmail.com>
2022-11-10 15:44:27 +0900

    sync

--- app/src/main/java/app/revanced/integrations/patches/HideEndscreenCardsPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/HideEndscreenCardsPatch.java
@@ -7,7 +7,7 @@ import app.revanced.integrations.settings.SettingsEnum;
 public class HideEndscreenCardsPatch {
     //Used by app.revanced.patches.youtube.layout.hideendscreencards.bytecode.patch.HideEndscreenCardsPatch
     public static void hideEndscreen(View view) {
-        if (SettingsEnum.ENDSCREEN_CARDS.getBoolean()) return;
+        if (SettingsEnum.ENDSCREEN_CARDS_SHOWN.getBoolean()) return;
         view.setVisibility(View.GONE);
     }
 }
