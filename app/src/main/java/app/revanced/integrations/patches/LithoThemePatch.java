5e5e2560788a98d4553f8be3878f511ba30c7c1e
OxrxL <108184954+OxrxL@users.noreply.github.com>
2022-11-13 06:36:42 +0100

    refactor(youtube/theme): changed recoloring logic (#3)

--- app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
+++ app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
@@ -7,25 +7,17 @@ import app.revanced.integrations.utils.ReVancedUtils;
 import app.revanced.integrations.utils.ThemeHelper;
 
 public class LithoThemePatch {
-    private static final int[] CONSTANTS = {
-        -15198184, // music related results panel background
-        -98492127, // video chapters list background
-        -14145496 // drawer content view background
-    };
-
     private static final int[] DARKCONSTANTS = {
-        -15790321 // comments chip background (new layout)
-    };
-
-    private static final int[] EXCEPTION = {
+        -14145496 // drawer content view background
         -14606047 // comments chip background
+        -15198184, // music related results panel background
+        -15790321 // comments chip background (new layout)
+        -98492127, // video chapters list background
     };
 
     // Used by app.revanced.patches.youtube.layout.theme.patch.LithoThemePatch
     public static int applyLithoTheme(int originalValue) {
-        Context context = ReVancedUtils.getContext();
-        if (anyEquals(originalValue, CONSTANTS) || (ThemeHelper.isDarkTheme() && anyEquals(originalValue, DARKCONSTANTS))) return 0;
-        else if (anyEquals(originalValue, EXCEPTION)) return context.getResources().getColor(context.getResources().getIdentifier("yt_black1", "color", context.getPackageName()));
+        if (ThemeHelper.isDarkTheme() && anyEquals(originalValue, DARKCONSTANTS) return -16777215;
         return originalValue;
     }
 
