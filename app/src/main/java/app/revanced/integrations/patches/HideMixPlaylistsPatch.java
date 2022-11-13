1a83cc4b544ee76d312f8e3c325bd3640fe11612
inotia00 <ars258000@gmail.com>
2022-11-13 17:39:58 +0900

    bump

--- app/src/main/java/app/revanced/integrations/patches/HideMixPlaylistsPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/HideMixPlaylistsPatch.java
@@ -2,12 +2,13 @@ package app.revanced.integrations.patches;
 
 import android.view.View;
 
+import app.revanced.integrations.adremover.AdRemoverAPI;
 import app.revanced.integrations.settings.SettingsEnum;
 
 public class HideMixPlaylistsPatch {
 
     public static void hideMixPlaylists(View view) {
         if (SettingsEnum.HIDE_MIX_PLAYLISTS.getBoolean()) return;
-        view.setVisibility(View.GONE);
+        AdRemoverAPI.HideViewWithLayout1dp(view);
     }
 }
--- app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
+++ app/src/main/java/app/revanced/integrations/patches/LithoThemePatch.java
@@ -4,20 +4,27 @@ import android.content.Context;
 import android.util.Log;
 
 import app.revanced.integrations.utils.ReVancedUtils;
-import app.revanced.integrations.utils.ThemeHelper;
 
 public class LithoThemePatch {
+    private static final int[] WHITECONSTANTS = {
+        -1, // comments chip background
+        -394759, // music related results panel background
+        -83886081 // video chapters list background
+    };
+
     private static final int[] DARKCONSTANTS = {
-        -14145496 // drawer content view background
-        -14606047 // comments chip background
+        -14145496, // drawer content view background
+        -14606047, // comments chip background
         -15198184, // music related results panel background
-        -15790321 // comments chip background (new layout)
-        -98492127, // video chapters list background
+        -15790321, // comments chip background (new layout)
+        -98492127 // video chapters list background
     };
 
     // Used by app.revanced.patches.youtube.layout.theme.patch.LithoThemePatch
     public static int applyLithoTheme(int originalValue) {
-        if (ThemeHelper.isDarkTheme() && anyEquals(originalValue, DARKCONSTANTS) return -16777215;
+        Context context = ReVancedUtils.getContext();
+        if (anyEquals(originalValue, DARKCONSTANTS)) return context.getResources().getColor(context.getResources().getIdentifier("yt_black1", "color", context.getPackageName()));
+        else if (anyEquals(originalValue, WHITECONSTANTS)) return context.getResources().getColor(context.getResources().getIdentifier("yt_white1", "color", context.getPackageName()));
         return originalValue;
     }
 
