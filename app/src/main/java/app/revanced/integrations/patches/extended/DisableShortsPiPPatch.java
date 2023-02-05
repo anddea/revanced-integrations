9f7bd7ff9d0c6dce8b49e5c249f412c3dbf32c68
inotia00 <ars258000@gmail.com>
2023-02-05 12:29:53 +0900

    refactor: `Disable pip mode in shorts player` (Experimental Flags) setting now works on Android 12+ devices

--- app/src/main/java/app/revanced/integrations/patches/extended/DisableShortsPiPPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/extended/DisableShortsPiPPatch.java
@@ -5,8 +5,8 @@ import app.revanced.integrations.shared.PlayerType;
 
 public class DisableShortsPiPPatch {
 
-    public static boolean disableShortsPlayerPiP() {
-        return SettingsEnum.DISABLE_SHORTS_PLAYER_PIP.getBoolean() && PlayerType.getCurrent() == PlayerType.NONE;
+    public static boolean disableShortsPlayerPiP(boolean original) {
+        return !(SettingsEnum.DISABLE_SHORTS_PLAYER_PIP.getBoolean() && PlayerType.getCurrent() == PlayerType.NONE) && original;
     }
 
 }
