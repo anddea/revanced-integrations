db3c469e8a0338a0dadec63ceb9dde11484274fe
inotia00 <ars258000@gmail.com>
2023-02-07 05:12:21 +0900

    refactor: `Disable pip mode in shorts player` (Experimental Flags) setting

--- app/src/main/java/app/revanced/integrations/patches/extended/DisableShortsPiPPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/extended/DisableShortsPiPPatch.java
@@ -1,12 +1,19 @@
 package app.revanced.integrations.patches.extended;
 
 import app.revanced.integrations.settings.SettingsEnum;
-import app.revanced.integrations.shared.PlayerType;
 
 public class DisableShortsPiPPatch {
+    private static boolean isShortsPlaying = false;
 
-    public static boolean disableShortsPlayerPiP(boolean original) {
-        return !(SettingsEnum.DISABLE_SHORTS_PLAYER_PIP.getBoolean() && PlayerType.getCurrent() == PlayerType.NONE) && original;
+    public static void generalPlayer() {
+        isShortsPlaying = false;
     }
 
+    public static void shortsPlayer() {
+        isShortsPlaying = true;
+    }
+
+    public static boolean disableShortsPlayerPiP(boolean original) {
+        return !(SettingsEnum.DISABLE_SHORTS_PLAYER_PIP.getBoolean() && isShortsPlaying) && original;
+    }
 }
