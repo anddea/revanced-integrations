8345147170a751f95c6c307dbaa8ad3c2d805ee2
inotia00 <ars258000@gmail.com>
2023-02-18 15:09:28 +0900

    add `save video speed` settings

--- app/src/main/java/app/revanced/integrations/patches/video/VideoSpeedPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/video/VideoSpeedPatch.java
@@ -1,37 +1,59 @@
 package app.revanced.integrations.patches.video;
 
+import static app.revanced.integrations.utils.SharedPrefHelper.SharedPrefNames.REVANCED;
+import static app.revanced.integrations.utils.SharedPrefHelper.getFloat;
+import static app.revanced.integrations.utils.SharedPrefHelper.saveString;
+import static app.revanced.integrations.utils.StringRef.str;
+
+import android.widget.Toast;
+
+import java.util.Objects;
+
 import app.revanced.integrations.settings.SettingsEnum;
+import app.revanced.integrations.utils.LogHelper;
+import app.revanced.integrations.utils.ReVancedUtils;
 import app.revanced.integrations.whitelist.Whitelist;
 
 public class VideoSpeedPatch {
+    private static final String DEFAULT_VIDEO_SPEED_PREFERENCE_KEY = "revanced_default_video_speed";
+
+    private static float selectedSpeed = -1.0f;
 
     private static boolean newVideo = false;
-    private static boolean userChangedSpeed = false;
-    private static String currentVideoId;
 
-    public static void userChangedSpeed(float speed) {
-        userChangedSpeed = true;
+    public static void userChangedSpeed(final float speed) {
+        selectedSpeed = speed;
+        var context = Objects.requireNonNull(ReVancedUtils.getContext());
+
+        if (SettingsEnum.ENABLE_SAVE_VIDEO_SPEED.getBoolean()) {
+            SettingsEnum.DEFAULT_VIDEO_SPEED.saveValue(speed);
+            saveString(context, REVANCED, DEFAULT_VIDEO_SPEED_PREFERENCE_KEY, speed + "");
+            Toast.makeText(context, str("revanced_save_video_speed") + speed + "x", Toast.LENGTH_SHORT).show();
+		}
     }
 
-    public static float getSpeedValue() {
-        if (!newVideo || userChangedSpeed) return -1.0f;
+    public static void setDefaultSpeed() {
+        float defaultSpeed = selectedSpeed;
+        var context = Objects.requireNonNull(ReVancedUtils.getContext());
+        if (newVideo) {
+            defaultSpeed = getFloat(context, REVANCED, DEFAULT_VIDEO_SPEED_PREFERENCE_KEY, -1.0f);
+            selectedSpeed = defaultSpeed;
 
-        float defaultSpeed = SettingsEnum.DEFAULT_VIDEO_SPEED.getFloat();
+            newVideo = false;
 
-        newVideo = false;
-        if (Whitelist.isChannelSPEEDWhitelisted()) defaultSpeed = 1.0f;
-
-        if (defaultSpeed == -2.0f) return -1.0f;
-        else if (!isCustomVideoSpeedEnabled() && defaultSpeed >= 2.0f) defaultSpeed = 2.0f;
-
-        return defaultSpeed;
+            if (Whitelist.isChannelSPEEDWhitelisted()) defaultSpeed = -1.0f;
+            else if (!isCustomVideoSpeedEnabled() && defaultSpeed >= 2.0f) defaultSpeed = 2.0f;
+        }
+        overrideSpeed(defaultSpeed);
     }
 
     public static void newVideoStarted(String videoId) {
-        if (videoId.equals(currentVideoId)) return;
-        currentVideoId = videoId;
         newVideo = true;
-        userChangedSpeed = false;
+    }
+
+    public static void overrideSpeed(final float speedValue) {
+        selectedSpeed = speedValue;
+        LogHelper.debug(VideoSpeedPatch.class, "Speed changed to: " + speedValue);
     }
 
     public static boolean isCustomVideoSpeedEnabled() {
