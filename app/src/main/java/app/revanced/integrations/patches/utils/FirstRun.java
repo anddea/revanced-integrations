1a53942a176d3dce94939922df3c4d8d51f13dd6
inotia00 <ars258000@gmail.com>
2023-02-19 00:02:55 +0900

    migrate with previous build

--- app/src/main/java/app/revanced/integrations/patches/utils/FirstRun.java
+++ app/src/main/java/app/revanced/integrations/patches/utils/FirstRun.java
@@ -1,7 +1,9 @@
 package app.revanced.integrations.patches.utils;
 
-import app.revanced.integrations.sponsorblock.PlayerController;
+import static app.revanced.integrations.utils.ReVancedHelper.setBuildVersion;
+
 import app.revanced.integrations.settings.SettingsEnum;
+import app.revanced.integrations.sponsorblock.PlayerController;
 
 public class FirstRun {
 
@@ -11,6 +13,10 @@ public class FirstRun {
         // set dummy video-id to initialize Sponsorblock: TeamVanced's Sponsorblock tutorial (https://www.youtube.com/watch?v=sE2IzSn-hHU)
         PlayerController.setCurrentVideoId("sE2IzSn-hHU");
         SettingsEnum.SB_FIRSTRUN.saveValue(true);
+    }
+
+    public static void initializationRVX() {
+        setBuildVersion();
     }
 
 }
--- app/src/main/java/app/revanced/integrations/utils/ReVancedHelper.java
+++ app/src/main/java/app/revanced/integrations/utils/ReVancedHelper.java
@@ -1,8 +1,20 @@
 package app.revanced.integrations.utils;
 
+import static app.revanced.integrations.utils.SharedPrefHelper.SharedPrefNames.REVANCED;
+import static app.revanced.integrations.utils.SharedPrefHelper.SharedPrefNames.YOUTUBE;
+import static app.revanced.integrations.utils.SharedPrefHelper.getString;
+import static app.revanced.integrations.utils.SharedPrefHelper.saveString;
+
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 
+import java.util.Objects;
+
+import app.revanced.integrations.BuildConfig;
+
 public class ReVancedHelper {
+    private static final String PREFERENCE_KEY = "integrations";
 
     private ReVancedHelper() {
     } // utility class
@@ -11,6 +23,20 @@ public class ReVancedHelper {
         var context = ReVancedUtils.getContext();
         assert context != null;
         return context.getResources().getConfiguration().smallestScreenWidthDp >= 600;
+    }
+
+    public static String getAppName() {
+        String appName = "ReVanced_Extended";
+        try {
+            var context = ReVancedUtils.getContext();
+            assert context != null;
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(), 0);
+            appName = packageInfo.applicationInfo.loadLabel(packageManager) + "";
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return appName;
     }
 
     public static String getVersionName() {
@@ -22,5 +48,26 @@ public class ReVancedHelper {
             e.printStackTrace();
         }
         return "17.49.37";
+    }
+
+    public static void setBuildVersion() {
+        var context = Objects.requireNonNull(ReVancedUtils.getContext());
+        var savedVersion = getString(context, YOUTUBE, PREFERENCE_KEY, null);
+        var currentVersion = BuildConfig.VERSION_NAME;
+
+        if (savedVersion == null) resetPreference(currentVersion);
+        else if (!savedVersion.equals(currentVersion))
+            saveString(context, YOUTUBE, PREFERENCE_KEY, currentVersion);
+    }
+
+    public static void resetPreference(String currentVersion) {
+        try {
+            var context = Objects.requireNonNull(ReVancedUtils.getContext());
+            SharedPreferences.Editor prefEdit = SharedPrefHelper.getPreferences(context, REVANCED).edit();
+            prefEdit.clear();
+            prefEdit.apply();
+            saveString(context, YOUTUBE, PREFERENCE_KEY, currentVersion);
+        } catch (Exception ignored) {
+        }
     }
 }
