8b91aed9956b51cf0f782df1df963d816b0f3f3c
inotia00 <ars258000@gmail.com>
2023-03-03 02:41:12 +0900

    refactor(enable-old-quality-layout): match with the official ReVanced's source code

--- app/src/main/java/app/revanced/integrations/patches/layout/FlyoutPanelLayoutPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/layout/FlyoutPanelLayoutPatch.java
@@ -1,17 +1,30 @@
 package app.revanced.integrations.patches.layout;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
 
 import app.revanced.integrations.settings.SettingsEnum;
-import app.revanced.integrations.utils.ReVancedUtils;
 
 public class FlyoutPanelLayoutPatch {
 
     public static void enableOldQualityMenu(ListView listView) {
         if (!SettingsEnum.ENABLE_OLD_QUALITY_LAYOUT.getBoolean()) return;
 
-        listView.setVisibility(View.GONE);
-        ReVancedUtils.runOnMainThread(() -> listView.performItemClick(null, 4, 0));
+        listView.setOnHierarchyChangeListener(new ViewGroup.OnHierarchyChangeListener() {
+            @Override
+            public void onChildViewAdded(View parent, View child) {
+                parent.setVisibility(View.GONE);
+
+                final var indexOfAdvancedQualityMenuItem = 4;
+                if (listView.indexOfChild(child) != indexOfAdvancedQualityMenuItem) return;
+
+                final var qualityItemMenuPosition = 4;
+                listView.performItemClick(null, qualityItemMenuPosition, 0);
+            }
+
+            @Override
+            public void onChildViewRemoved(View parent, View child) {}
+        });
     }
 }
