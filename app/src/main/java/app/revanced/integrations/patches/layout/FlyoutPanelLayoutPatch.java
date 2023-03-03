c5759f949c5faf69a5d58bb9c57ac9f8eaf0309a
inotia00 <ars258000@gmail.com>
2023-03-03 23:09:19 +0900

    refactor(enable-old-quality-layout): improve the patch method

--- app/src/main/java/app/revanced/integrations/patches/layout/FlyoutPanelLayoutPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/layout/FlyoutPanelLayoutPatch.java
@@ -1,7 +1,8 @@
 package app.revanced.integrations.patches.layout;
 
+import static app.revanced.integrations.utils.ReVancedUtils.runOnMainThread;
+
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ListView;
 
 import app.revanced.integrations.settings.SettingsEnum;
@@ -11,20 +12,7 @@ public class FlyoutPanelLayoutPatch {
     public static void enableOldQualityMenu(ListView listView) {
         if (!SettingsEnum.ENABLE_OLD_QUALITY_LAYOUT.getBoolean()) return;
 
-        listView.setOnHierarchyChangeListener(new ViewGroup.OnHierarchyChangeListener() {
-            @Override
-            public void onChildViewAdded(View parent, View child) {
-                parent.setVisibility(View.GONE);
-
-                final var indexOfAdvancedQualityMenuItem = 4;
-                if (listView.indexOfChild(child) != indexOfAdvancedQualityMenuItem) return;
-
-                final var qualityItemMenuPosition = 4;
-                listView.performItemClick(null, qualityItemMenuPosition, 0);
-            }
-
-            @Override
-            public void onChildViewRemoved(View parent, View child) {}
-        });
+        listView.setVisibility(View.GONE);
+        runOnMainThread(() -> listView.performItemClick(null, 4, 0));
     }
 }
