27fd415906078130573053845f47fa9e47aa73b6
inotia00 <ars258000@gmail.com>
2023-03-05 19:46:00 +0900

    refactor(enable-old-quality-layout): improve the patch method

--- app/src/main/java/app/revanced/integrations/patches/layout/FlyoutPanelLayoutPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/layout/FlyoutPanelLayoutPatch.java
@@ -1,7 +1,7 @@
 package app.revanced.integrations.patches.layout;
 
-import static app.revanced.integrations.utils.ReVancedUtils.runOnMainThread;
-
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.ListView;
 
@@ -11,9 +11,12 @@ public class FlyoutPanelLayoutPatch {
 
     public static void enableOldQualityMenu(ListView listView) {
         if (!SettingsEnum.ENABLE_OLD_QUALITY_LAYOUT.getBoolean()) return;
-
         listView.setVisibility(View.GONE);
-        runOnMainThread(() -> listView.performItemClick(null, 4, 0));
+
+        new Handler(Looper.getMainLooper()).postDelayed(() ->
+                        listView.performItemClick(null, 4, 0),
+                1
+        );
     }
 
     public static int enableOldQualityLayout(int original) {
