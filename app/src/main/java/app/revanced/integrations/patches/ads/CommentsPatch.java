9b59ae98293458236b2a8f5bca947488c87595ea
inotia00 <ars258000@gmail.com>
2023-03-18 23:04:13 +0900

    add `Hide emoji picker`(in comments section)

--- app/src/main/java/app/revanced/integrations/patches/ads/CommentsPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/ads/CommentsPatch.java
@@ -8,11 +8,13 @@ final class CommentsPatch extends Filter {
         var comments = new BlockRule(SettingsEnum.HIDE_COMMENTS_SECTION, "video_metadata_carousel", "_comments");
         var previewComment = new BlockRule(SettingsEnum.HIDE_PREVIEW_COMMENT, "|carousel_item", "comments_entry_point_teaser", "comments_entry_point_simplebox");
         var thanksButton = new BlockRule(SettingsEnum.HIDE_COMMENTS_THANKS_BUTTON, "super_thanks_button");
+        var emojiPicker = new BlockRule(SettingsEnum.HIDE_EMOJI_PICKER, "|CellType|ContainerType|ContainerType|ContainerType|ContainerType|ContainerType|");
 
         this.pathRegister.registerAll(
                 comments,
                 previewComment,
-                thanksButton
+                thanksButton,
+                emojiPicker
         );
     }
 
