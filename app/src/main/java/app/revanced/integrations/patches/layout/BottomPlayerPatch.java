ea8459876f890aec0792beda5c78916a02106e10
inotia00 <ars258000@gmail.com>
2023-04-03 18:33:31 +0900

    rollback(hide-button-container): restore to old code

--- app/src/main/java/app/revanced/integrations/patches/layout/BottomPlayerPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/layout/BottomPlayerPatch.java
@@ -3,9 +3,6 @@ package app.revanced.integrations.patches.layout;
 import static app.revanced.integrations.patches.ads.ByteBufferFilterPatch.indexOf;
 
 import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 
 import app.revanced.integrations.settings.SettingsEnum;
 
@@ -13,28 +10,38 @@ public class BottomPlayerPatch {
 
     public static boolean hideActionButtons(Object object, ByteBuffer buffer) {
         String value = object.toString();
-        if (!value.contains("ContainerType|ContainerType|video_action_button"))
+        if (!value.contains("ScrollableContainerType|ContainerType|ContainerType|video_action_button"))
             return false;
 
-        String convertedBuffer = new String(buffer.array(), StandardCharsets.UTF_8);
+        if (SettingsEnum.HIDE_LIVE_CHAT_BUTTON.getBoolean()) {
+            int bufferIndex = indexOf(buffer.array(), "yt_outline_message_bubble_overlap".getBytes());
+            if (bufferIndex > 0 && bufferIndex < 2500) return true;
+        }
+        if (SettingsEnum.HIDE_SHARE_BUTTON.getBoolean()) {
+            int bufferIndex = indexOf(buffer.array(), "yt_outline_share".getBytes());
+            if (bufferIndex > 0 && bufferIndex < 2500) return true;
+        }
+        if (SettingsEnum.HIDE_SHOP_BUTTON.getBoolean()) {
+            int bufferIndex = indexOf(buffer.array(), "yt_outline_bag".getBytes());
+            if (bufferIndex > 0 && bufferIndex < 2500) return true;
+        }
+        if (SettingsEnum.HIDE_REPORT_BUTTON.getBoolean()) {
+            int bufferIndex = indexOf(buffer.array(), "yt_outline_flag".getBytes());
+            if (bufferIndex > 0 && bufferIndex < 2500) return true;
+        }
+        if (SettingsEnum.HIDE_REMIX_BUTTON.getBoolean()) {
+            int bufferIndex = indexOf(buffer.array(), "yt_outline_youtube_shorts_plus".getBytes());
+            if (bufferIndex > 0 && bufferIndex < 2500) return true;
+        }
+        if (SettingsEnum.HIDE_THANKS_BUTTON.getBoolean()) {
+            int bufferIndex = indexOf(buffer.array(), "yt_outline_dollar_sign_heart".getBytes());
+            if (bufferIndex > 0 && bufferIndex < 2500) return true;
+        }
+        if (SettingsEnum.HIDE_CREATE_CLIP_BUTTON.getBoolean()) {
+            int bufferIndex = indexOf(buffer.array(), "yt_outline_scissors".getBytes());
+            if (bufferIndex > 0 && bufferIndex < 2500) return true;
+        }
 
-        List<String> actionButtonsBlockList = new ArrayList<>();
-
-        if (SettingsEnum.HIDE_LIVE_CHAT_BUTTON.getBoolean())
-            actionButtonsBlockList.add("yt_outline_message_bubble_overlap");
-        if (SettingsEnum.HIDE_SHARE_BUTTON.getBoolean())
-            actionButtonsBlockList.add("yt_outline_share");
-        if (SettingsEnum.HIDE_SHOP_BUTTON.getBoolean())
-            actionButtonsBlockList.add("yt_outline_bag");
-        if (SettingsEnum.HIDE_REPORT_BUTTON.getBoolean())
-            actionButtonsBlockList.add("yt_outline_flag");
-        if (SettingsEnum.HIDE_REMIX_BUTTON.getBoolean())
-            actionButtonsBlockList.add("yt_outline_youtube_shorts_plus");
-        if (SettingsEnum.HIDE_THANKS_BUTTON.getBoolean())
-            actionButtonsBlockList.add("yt_outline_dollar_sign_heart");
-        if (SettingsEnum.HIDE_CREATE_CLIP_BUTTON.getBoolean())
-            actionButtonsBlockList.add("yt_outline_scissors");
-
-        return actionButtonsBlockList.stream().anyMatch(convertedBuffer::contains);
+        return false;
     }
 }
