0345a3fba8f33e7712c5371be98ac70a0e4f71ff
inotia00 <ars258000@gmail.com>
2023-04-03 18:31:13 +0900

    fix: buildToolsVersion does not match

--- dummy/build.gradle.kts
+++ dummy/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "app.revanced.dummy"
     compileSdk = 33
-    buildToolsVersion = "33.0.1"
+    buildToolsVersion = "33.0.2"
 
     defaultConfig {
         minSdk = 26
