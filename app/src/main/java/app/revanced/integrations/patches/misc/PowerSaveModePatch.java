a40583acb542f2fc2a6112acc3b78129c79a123a
inotia00 <ars258000@gmail.com>
2023-04-03 18:35:53 +0900

    add `bypass-ambient-mode-restrictions` patch

---
+++ app/src/main/java/app/revanced/integrations/patches/misc/PowerSaveModePatch.java
@@ -0,0 +1,10 @@
+package app.revanced.integrations.patches.misc;
+
+import app.revanced.integrations.settings.SettingsEnum;
+
+public class PowerSaveModePatch {
+
+    public static boolean bypassPowerSaveModeRestrictions(boolean original) {
+        return !SettingsEnum.BYPASS_AMBIENT_MODE_RESTRICTIONS.getBoolean() && original;
+    }
+}
