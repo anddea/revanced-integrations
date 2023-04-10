f691231b6045663187e983aad128b7ac0dfb99f0
inotia00 <ars258000@gmail.com>
2023-04-11 06:05:15 +0900

    fix(settings): dark theme not applied in ReVanced settings https://github.com/inotia00/ReVanced_Extended/issues/690

--- app/src/main/java/app/revanced/integrations/utils/ThemeHelper.kt
+++ app/src/main/java/app/revanced/integrations/utils/ThemeHelper.kt
@@ -5,32 +5,12 @@ import app.revanced.integrations.utils.ResourceUtils.identifier
 object ThemeHelper {
     private const val PRIMARY_DARK_THEME = "Theme.YouTube.Settings.Dark"
     private const val PRIMARY_WHITE_THEME = "Theme.YouTube.Settings"
-    private const val SECONDARY_DARK_THEME = "Theme.YouTube.Dark.DarkerPalette"
-    private const val SECONDARY_WHITE_THEME = "Theme.YouTube.Light.DarkerPalette"
 
     private var isDarkTheme = 0
 
     @JvmStatic
     fun setTheme(value: Any) {
         isDarkTheme = (value as Enum<*>).ordinal
-    }
-
-    @JvmStatic
-    fun setPrimaryTheme(theme: Int) {
-        isDarkTheme = if (matchTheme(theme, PRIMARY_DARK_THEME)) 1 else if (matchTheme(
-                theme,
-                PRIMARY_WHITE_THEME
-            )
-        ) 0 else isDarkTheme
-    }
-
-    @JvmStatic
-    fun setSecondaryTheme(theme: Int) {
-        isDarkTheme = if (matchTheme(theme, SECONDARY_DARK_THEME)) 1 else if (matchTheme(
-                theme,
-                SECONDARY_WHITE_THEME
-            )
-        ) 0 else isDarkTheme
     }
 
     @JvmStatic
@@ -43,10 +23,4 @@ object ThemeHelper {
             val themeName = if (dayNightTheme) PRIMARY_DARK_THEME else PRIMARY_WHITE_THEME
             return identifier(themeName, ResourceType.STYLE)
         }
-
-    @JvmStatic
-    fun matchTheme(themeValue: Int, themeName: String?): Boolean {
-        return themeValue == themeName?.let { identifier(it, ResourceType.STYLE) }
-    }
-
 }
