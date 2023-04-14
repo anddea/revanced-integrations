0ea860f4695ef32bd179bab6b6fea198f2409e64
inotia00 <ars258000@gmail.com>
2023-04-14 11:28:48 +0900

    refactor(default-video-quality): apply the official ReVanced commit

--- app/src/main/java/app/revanced/integrations/patches/video/VideoQualityPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/video/VideoQualityPatch.java
@@ -1,119 +1,126 @@
 package app.revanced.integrations.patches.video;
 
+import static app.revanced.integrations.utils.ReVancedUtils.NetworkType;
 import static app.revanced.integrations.utils.ReVancedUtils.getNetworkType;
 import static app.revanced.integrations.utils.ReVancedUtils.showToastShort;
-import static app.revanced.integrations.utils.SharedPrefHelper.SharedPrefNames.REVANCED;
-import static app.revanced.integrations.utils.SharedPrefHelper.getInt;
-import static app.revanced.integrations.utils.SharedPrefHelper.saveString;
 import static app.revanced.integrations.utils.StringRef.str;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.List;
 
 import app.revanced.integrations.settings.SettingsEnum;
 import app.revanced.integrations.utils.LogHelper;
-import app.revanced.integrations.utils.ReVancedUtils;
 
 public class VideoQualityPatch {
+    private static final int AUTOMATIC_VIDEO_QUALITY_VALUE = -2;
+    private static final SettingsEnum wifiQualitySetting = SettingsEnum.DEFAULT_VIDEO_QUALITY_WIFI;
+    private static final SettingsEnum mobileQualitySetting = SettingsEnum.DEFAULT_VIDEO_QUALITY_MOBILE;
 
-    public static int selectedQuality1 = -2;
-    private static boolean newVideo = false;
-    private static boolean userChangedQuality = false;
+    private static boolean qualityNeedsUpdating;
+    @Nullable
+    private static String currentVideoId;
 
-    public static void changeDefaultQuality(int defaultQuality) {
-        if (SettingsEnum.ENABLE_SAVE_VIDEO_QUALITY.getBoolean()) {
-            var networkType = getNetworkType();
-            var preferenceKey = "revanced_default_video_quality_" + networkType.getName();
-            var toastMessage = str("revanced_save_video_quality_" + networkType.getName());
-            var changedMessage = toastMessage + "\u2009" + defaultQuality + "p";
+    private static boolean userChangedDefaultQuality;
 
-            if (networkType == ReVancedUtils.NetworkType.NONE) {
-                showToastShort(toastMessage);
-            } else {
-                saveString(REVANCED, preferenceKey, defaultQuality + "");
-                showToastShort(changedMessage);
-            }
+    private static int userSelectedQualityIndex;
+
+    @Nullable
+    private static List<Integer> videoQualities;
+
+    private static void changeDefaultQuality(int defaultQuality) {
+        var networkType = getNetworkType();
+        var toastMessage = str("revanced_save_video_quality_" + networkType.getName());
+        var changedMessage = toastMessage + "\u2009" + defaultQuality + "p";
+
+        if (networkType == NetworkType.NONE) {
+            showToastShort(str("revanced_save_video_quality_none"));
+            return;
         }
-        userChangedQuality = false;
+        if (networkType == NetworkType.MOBILE) {
+            mobileQualitySetting.saveValue(defaultQuality);
+        } else {
+            wifiQualitySetting.saveValue(defaultQuality);
+        }
+        showToastShort(changedMessage);
     }
 
-    public static int setVideoQuality(Object[] qualities, int quality, Object qInterface, String qIndexMethod) {
-        int defaultQuality;
-
-        if (!(newVideo || userChangedQuality) || qInterface == null) return quality;
-
-        Class<?> intType = Integer.TYPE;
-        ArrayList<Integer> iStreamQualities = new ArrayList<>();
+    public static int setVideoQuality(Object[] qualities, final int originalQualityIndex, Object qInterface, String qIndexMethod) {
         try {
-            for (Object streamQuality : qualities) {
-                for (Field field : streamQuality.getClass().getFields()) {
-                    if (field.getType().isAssignableFrom(intType)) {  // converts quality index to actual readable resolution
-                        int value = field.getInt(streamQuality);
-                        if (field.getName().length() <= 2) {
-                            iStreamQualities.add(value);
+            if (!(qualityNeedsUpdating || userChangedDefaultQuality) || qInterface == null) {
+                return originalQualityIndex;
+            }
+            qualityNeedsUpdating = false;
+
+            final int preferredQuality;
+            if (getNetworkType() == NetworkType.MOBILE) {
+                preferredQuality = mobileQualitySetting.getInt();
+            } else {
+                preferredQuality = wifiQualitySetting.getInt();
+            }
+            if (!userChangedDefaultQuality && preferredQuality == AUTOMATIC_VIDEO_QUALITY_VALUE) {
+                return originalQualityIndex; // nothing to do
+            }
+
+            if (videoQualities == null || videoQualities.size() != qualities.length) {
+                videoQualities = new ArrayList<>(qualities.length);
+                for (Object streamQuality : qualities) {
+                    for (Field field : streamQuality.getClass().getFields()) {
+                        if (field.getType().isAssignableFrom(Integer.TYPE)
+                                && field.getName().length() <= 2) {
+                            videoQualities.add(field.getInt(streamQuality));
                         }
                     }
                 }
             }
-        } catch (Exception ignored) {
-        }
 
-        Collections.sort(iStreamQualities);
-        int index = 0;
-        if (userChangedQuality) {
-            for (int convertedQuality : iStreamQualities) {
-                int selectedQuality2 = qualities.length - selectedQuality1 + 1;
-                index++;
-                if (selectedQuality2 == index) {
-                    changeDefaultQuality(convertedQuality);
-                    return selectedQuality2;
+            if (userChangedDefaultQuality) {
+                userChangedDefaultQuality = false;
+                final int quality = videoQualities.get(userSelectedQualityIndex);
+                changeDefaultQuality(quality);
+                return userSelectedQualityIndex;
+            }
+
+            // find the highest quality that is equal to or less than the preferred
+            int qualityToUse = videoQualities.get(0); // first element is automatic mode
+            int qualityIndexToUse = 0;
+            int i = 0;
+            for (Integer quality : videoQualities) {
+                if (quality <= preferredQuality && qualityToUse < quality)  {
+                    qualityToUse = quality;
+                    qualityIndexToUse = i;
                 }
+                i++;
             }
-        }
+            if (qualityIndexToUse == originalQualityIndex)
+                return originalQualityIndex;
 
-        newVideo = false;
-        var networkType = getNetworkType();
-        var preferenceKey = "revanced_default_video_quality_" + networkType.getName();
-
-        if (networkType == ReVancedUtils.NetworkType.NONE) return quality;
-        else defaultQuality = getInt(REVANCED, preferenceKey, -2);
-
-        if (defaultQuality == -2) return quality;
-
-        for (int ignored : iStreamQualities) {
-            index++;
-        }
-        for (Integer iStreamQuality : iStreamQualities) {
-            int streamQuality3 = iStreamQuality;
-            if (streamQuality3 <= defaultQuality) {
-                quality = streamQuality3;
-            }
-        }
-        if (quality == -2) return quality;
-
-        int qualityIndex = iStreamQualities.indexOf(quality);
-        try {
-            Class<?> cl = qInterface.getClass();
-            Method m = cl.getMethod(qIndexMethod, Integer.TYPE);
-            m.invoke(qInterface, iStreamQualities.get(qualityIndex));
-            return qualityIndex;
+            Method m = qInterface.getClass().getMethod(qIndexMethod, Integer.TYPE);
+            m.invoke(qInterface, qualityToUse);
+            return qualityIndexToUse;
         } catch (Exception ex) {
-            LogHelper.printException(VideoQualityPatch.class, "Failed to set quality", ex);
-            showToastShort(str("revanced_save_video_quality_common_error"));
-            return qualityIndex;
+            LogHelper.printException(VideoQualityPatch.class, str("revanced_save_video_quality_none"), ex);
+            return originalQualityIndex;
         }
     }
 
     public static void userChangedQuality(int selectedQuality) {
-        selectedQuality1 = selectedQuality;
-        userChangedQuality = true;
+        if (!SettingsEnum.ENABLE_SAVE_VIDEO_QUALITY.getBoolean()) return;
+
+        userSelectedQualityIndex = selectedQuality;
+        userChangedDefaultQuality = true;
     }
 
     public static void newVideoStarted(@NonNull String videoId) {
-        newVideo = true;
+        qualityNeedsUpdating = true;
+
+        if (!videoId.equals(currentVideoId)) {
+            currentVideoId = videoId;
+            videoQualities = null;
+        }
     }
 }
