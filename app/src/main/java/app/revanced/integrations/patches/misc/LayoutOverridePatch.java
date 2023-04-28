eabce388112e2d5c1eff2a5d5ae91969deac9cf7
inotia00 <ars258000@gmail.com>
2023-04-28 16:25:11 +0900

    refactor(layout-switch): community posts in channels are no longer hidden when `Enable tablet layout` is on

--- app/src/main/java/app/revanced/integrations/patches/misc/LayoutOverridePatch.java
+++ app/src/main/java/app/revanced/integrations/patches/misc/LayoutOverridePatch.java
@@ -10,16 +10,21 @@ public class LayoutOverridePatch {
      * Context is overridden when trying to play a YouTube video from the Google Play Store,
      * Which is speculated to affect LayoutOverridePatch
      */
+    public static boolean enableTabletLayout() {
+        try {
+            return getBoolean(REVANCED, "revanced_enable_tablet_layout", false);
+        } catch (Exception ex){
+            LogHelper.printException(LayoutOverridePatch.class, "Failed to getBoolean", ex);
+            return false;
+        }
+    }
+
     public static int getLayoutOverride(int original) {
         try {
-            boolean isTabletLayoutEnabled = getBoolean(REVANCED, "revanced_enable_tablet_layout", false);
-            boolean isPhoneLayoutEnabled = getBoolean(REVANCED, "revanced_enable_phone_layout", false);
-
-            return isTabletLayoutEnabled ? 720 : (isPhoneLayoutEnabled ? 480 : original);
+            return getBoolean(REVANCED, "revanced_enable_phone_layout", false) ? 480 : original;
         } catch (Exception ex){
             LogHelper.printException(LayoutOverridePatch.class, "Failed to getBoolean", ex);
             return original;
         }
-
     }
 }
