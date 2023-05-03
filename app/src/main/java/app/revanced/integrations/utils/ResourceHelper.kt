13da3a7a6fb4539bf78218e80aa1d0040e76c952
inotia00 <ars258000@gmail.com>
2023-05-03 23:35:48 +0900

    refactor(hide-player-button-background): apply better patch method

--- app/src/main/java/app/revanced/integrations/utils/ResourceHelper.kt
+++ app/src/main/java/app/revanced/integrations/utils/ResourceHelper.kt
@@ -9,24 +9,19 @@ import app.revanced.integrations.utils.ResourceUtils.identifier
 import app.revanced.integrations.utils.ThemeHelper.dayNightTheme
 
 object ResourceHelper {
+    private val PLAYER_CONTROL_BUTTON_LIST = listOf(
+        "player_control_fast_forward_button",
+        "player_control_next_button",
+        "play_button",
+        "player_control_play_pause_replay_button",
+        "player_control_previous_button",
+        "player_control_rewind_button"
+    )
+
     private const val ARROW_BLACK_ICON = "yt_outline_arrow_left_black_24"
     private const val ARROW_WHITE_ICON = "yt_outline_arrow_left_white_24"
 
-    private const val TRASH_BLACK_ICON = "yt_outline_trash_can_black_24"
-    private const val TRASH_WHITE_ICON = "yt_outline_trash_can_white_24"
-
     private const val COLLAPSE_BUTTON = "player_collapse_button"
-
-    private const val FAST_FORWARD_BUTTON = "player_control_fast_forward_button"
-
-    private const val NEXT_BUTTON = "player_control_next_button"
-
-    private const val PLAY_BUTTON = "play_button"
-    private const val PLAY_PAUSE_BUTTON = "player_control_play_pause_replay_button"
-
-    private const val PREVIOUS_BUTTON = "player_control_previous_button"
-
-    private const val REWIND_BUTTON = "player_control_rewind_button"
 
     @JvmStatic
     val resources: Resources get() = ReVancedUtils.getContext().resources
@@ -39,27 +34,20 @@ object ResourceHelper {
         }
 
     @JvmStatic
-    val trash: Int
-        get() {
-            val themeName = if (dayNightTheme) TRASH_WHITE_ICON else TRASH_BLACK_ICON
-            return identifier(themeName, ResourceType.DRAWABLE)
-        }
-
-    @JvmStatic
     fun hideCollapseButton(view: View): Boolean {
         return SettingsEnum.HIDE_COLLAPSE_BUTTON.boolean && (view.id == identifier(COLLAPSE_BUTTON, ResourceType.ID))
     }
 
     @JvmStatic
-    fun hidePlayerButtonBackground(view: View) {
-        if (SettingsEnum.HIDE_PLAYER_BUTTON_BACKGROUND.boolean
-            && (view.id == identifier(FAST_FORWARD_BUTTON, ResourceType.ID)
-                    || view.id == identifier(NEXT_BUTTON, ResourceType.ID)
-                    || view.id == identifier(PLAY_BUTTON, ResourceType.ID)
-                    || view.id == identifier(PLAY_PAUSE_BUTTON, ResourceType.ID)
-                    || view.id == identifier(PREVIOUS_BUTTON, ResourceType.ID)
-                    || view.id == identifier(REWIND_BUTTON, ResourceType.ID))) {
-            view.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    @Suppress("DEPRECATION")
+    fun hidePlayerButtonBackground(view: View?) {
+        if (view == null || !SettingsEnum.HIDE_PLAYER_BUTTON_BACKGROUND.boolean) return
+        for (blockList in PLAYER_CONTROL_BUTTON_LIST) {
+            if (view.id == identifier(blockList, ResourceType.ID)) view.setBackgroundDrawable(
+                ColorDrawable(
+                    Color.TRANSPARENT
+                )
+            )
         }
     }
 
