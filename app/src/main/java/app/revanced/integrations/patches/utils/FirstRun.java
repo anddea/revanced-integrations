2e8e83e80998dcb71928ed9fa71889110c38f466
inotia00 <ars258000@gmail.com>
2023-05-08 09:41:47 +0900

    feat(youtube): add reboot dialog when installing for the first time

--- app/src/main/java/app/revanced/integrations/patches/utils/FirstRun.java
+++ app/src/main/java/app/revanced/integrations/patches/utils/FirstRun.java
@@ -1,15 +1,25 @@
 package app.revanced.integrations.patches.utils;
 
 import static app.revanced.integrations.sponsorblock.SegmentPlaybackController.initialize;
-import static app.revanced.integrations.utils.ReVancedHelper.setBuildVersion;
+import static app.revanced.integrations.utils.ReVancedUtils.runOnMainThreadDelayed;
+import static app.revanced.integrations.utils.SharedPrefHelper.SharedPrefNames.YOUTUBE;
+import static app.revanced.integrations.utils.SharedPrefHelper.getString;
+import static app.revanced.integrations.utils.SharedPrefHelper.saveString;
+import static app.revanced.integrations.utils.StringRef.str;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
 
+import java.util.Objects;
+
+import app.revanced.integrations.BuildConfig;
 import app.revanced.integrations.settings.SettingsEnum;
 
 public class FirstRun {
+    private static final String PREFERENCE_KEY = "integrations";
 
     public static void initializationSB(@NonNull Context context) {
         if (SettingsEnum.SB_FIRST_RUN.getBoolean()) return;
@@ -18,7 +28,27 @@ public class FirstRun {
     }
 
     public static void initializationRVX(@NonNull Context context) {
-        setBuildVersion();
+        var integrationVersion = getString(YOUTUBE, PREFERENCE_KEY, null);
+
+        if (!Objects.equals(integrationVersion, BuildConfig.VERSION_NAME))
+            saveString(YOUTUBE, PREFERENCE_KEY, BuildConfig.VERSION_NAME);
+
+        if (integrationVersion != null) return;
+
+        Activity activity = (Activity) context;
+
+        new AlertDialog.Builder(activity)
+                .setMessage(str("revanced_reboot_first_run"))
+                .setPositiveButton(str("in_app_update_restart_button"), (dialog, id) ->
+                        runOnMainThreadDelayed(() ->{
+                            activity.finishAffinity();
+                            activity.startActivity(activity.getPackageManager().getLaunchIntentForPackage(activity.getPackageName()));
+                            System.exit(0);
+                            }, 1000L)
+                )
+                .setNegativeButton(str("sign_in_cancel"), null)
+                .setCancelable(false)
+                .show();
     }
 
 }
