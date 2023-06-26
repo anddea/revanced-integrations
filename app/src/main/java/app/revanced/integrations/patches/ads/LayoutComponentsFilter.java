7a8e53254295a982ac57d409fb0e2cf48ca3b769
inotia00 <ars258000@gmail.com>
2023-06-27 01:55:40 +0900

    feat(youtube/hide-layout-components): rollback commits for hide expandable chips

--- app/src/main/java/app/revanced/integrations/patches/ads/LayoutComponentsFilter.java
+++ app/src/main/java/app/revanced/integrations/patches/ads/LayoutComponentsFilter.java
@@ -43,6 +43,7 @@ public final class LayoutComponentsFilter extends Filter {
 
         final var expandableMetadata = new StringFilterGroup(
                 SettingsEnum.HIDE_EXPANDABLE_CHIP,
+                "expandable_metadata",
                 "inline_expander"
         );
 
