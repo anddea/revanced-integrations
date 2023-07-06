aaa0ec4a678000c62a32de93967f11e9c09b68b0
inotia00 <ars258000@gmail.com>
2023-07-06 11:50:29 +0900

    feat(reddit/hide-navigation-buttons): split into `hide-discover-button`, `hide-chat-button`, `hide-create-button`

--- app/src/main/java/app/revanced/reddit/settingsmenu/SettingsStatus.java
+++ app/src/main/java/app/revanced/reddit/settingsmenu/SettingsStatus.java
@@ -1,14 +1,29 @@
 package app.revanced.reddit.settingsmenu;
 
+import app.revanced.reddit.settings.SettingsEnum;
+
 public class SettingsStatus {
-    public static boolean navigationButtons = false;
+    public static boolean chatButtons = false;
+    public static boolean createButtons = false;
+    public static boolean discoverButtons = false;
     public static boolean openLinksDirectly = false;
     public static boolean openLinksExternally = false;
     public static boolean sanitizeUrlQuery = false;
     public static boolean screenshotPopup = false;
 
-    public static void NavigationButtons() {
-        navigationButtons = true;
+    public static void ChatButtons() {
+        chatButtons = true;
+        SettingsEnum.HIDE_CHAT_BUTTON.saveValue(true);
+    }
+
+    public static void CreateButtons() {
+        createButtons = true;
+        SettingsEnum.HIDE_CREATE_BUTTON.saveValue(true);
+    }
+
+    public static void DiscoverButtons() {
+        discoverButtons = true;
+        SettingsEnum.HIDE_DISCOVER_BUTTON.saveValue(true);
     }
 
     public static void OpenLinksDirectly() {
