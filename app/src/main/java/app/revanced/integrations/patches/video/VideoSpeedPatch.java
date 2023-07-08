93ee34ca915079d28aedceb29fff32582d15d5c9
inotia00 <ars258000@gmail.com>
2023-07-09 08:27:15 +0900

    fix(youtube/default-video-speed): unable to play video from PlayStore

--- app/src/main/java/app/revanced/integrations/patches/video/VideoSpeedPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/video/VideoSpeedPatch.java
@@ -1,28 +1,36 @@
 package app.revanced.integrations.patches.video;
 
 import static app.revanced.integrations.utils.ReVancedUtils.showToastShort;
+import static app.revanced.integrations.utils.SharedPrefHelper.SharedPrefNames.REVANCED;
+import static app.revanced.integrations.utils.SharedPrefHelper.getBoolean;
+import static app.revanced.integrations.utils.SharedPrefHelper.getFloat;
 import static app.revanced.integrations.utils.StringRef.str;
 
 import java.util.Objects;
 
 import app.revanced.integrations.settings.SettingsEnum;
+import app.revanced.integrations.utils.LogHelper;
 
 public class VideoSpeedPatch {
-    private static float selectedSpeed = -1.0f;
+    private static float selectedSpeed = 1.0f;
     private static String currentContentCpn;
 
     public static void newVideoStarted(final String contentCpn, final boolean isLive) {
-        if (contentCpn.isEmpty() || Objects.equals(currentContentCpn, contentCpn))
-            return;
+        try {
+            if (contentCpn.isEmpty() || Objects.equals(currentContentCpn, contentCpn))
+                return;
 
-        currentContentCpn = contentCpn;
+            currentContentCpn = contentCpn;
 
-        if (SettingsEnum.DISABLE_DEFAULT_VIDEO_SPEED_LIVE.getBoolean() && isLive)
-            return;
+            if (getBoolean(REVANCED, "revanced_disable_default_video_speed_live", true) && isLive)
+                return;
 
-        selectedSpeed = SettingsEnum.DEFAULT_VIDEO_SPEED.getFloat();
+            selectedSpeed = getFloat(REVANCED, "revanced_default_video_speed", 1.0f);
 
-        overrideSpeed(selectedSpeed);
+            overrideSpeed(selectedSpeed);
+        } catch (Exception ex) {
+            LogHelper.printException(VideoSpeedPatch.class, "Failed to setDefaultVideoSpeed", ex);
+        }
     }
 
     public static void userChangedSpeed(final float speed) {
