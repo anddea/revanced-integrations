6c36cfccdb57d7a9f9193239d000b4e0fd4b9e8c
inotia00 <ars258000@gmail.com>
2023-07-23 01:24:41 +0900

    fix(youtube/new-splash-animation): opening youtube link is buggy

--- app/src/main/java/app/revanced/integrations/patches/misc/SplashAnimationPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/misc/SplashAnimationPatch.java
@@ -1,10 +1,22 @@
 package app.revanced.integrations.patches.misc;
 
-import app.revanced.integrations.settings.SettingsEnum;
+import static app.revanced.integrations.utils.SharedPrefHelper.SharedPrefNames.REVANCED;
+import static app.revanced.integrations.utils.SharedPrefHelper.getBoolean;
+
+import app.revanced.integrations.utils.LogHelper;
 
 public class SplashAnimationPatch {
 
+    /**
+     * Context overrides when you open YouTube in an external browser.
+     * This is expected to affect SplashAnimationPatch.
+     */
     public static boolean enableNewSplashAnimation() {
-        return SettingsEnum.ENABLE_NEW_SPLASH_ANIMATION.getBoolean();
+        try {
+            return getBoolean(REVANCED, "revanced_enable_new_splash_animation", false);
+        } catch (Exception ex) {
+            LogHelper.printException(SplashAnimationPatch.class, "Failed to load enableNewSplashAnimation", ex);
+        }
+        return false;
     }
 }
