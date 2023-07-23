38a29194bdf5933a5dda657d59ff589bd36f70fd
inotia00 <ars258000@gmail.com>
2023-07-23 15:13:17 +0900

    fix(reddit/open-links-externally): exceptions occur in some manufacturers ROM

--- app/src/main/java/app/revanced/reddit/patches/OpenLinksExternallyPatch.java
+++ app/src/main/java/app/revanced/reddit/patches/OpenLinksExternallyPatch.java
@@ -3,11 +3,6 @@ package app.revanced.reddit.patches;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Parcelable;
-
-import androidx.annotation.NonNull;
-
-import com.reddit.frontpage.RedditDeepLinkActivity;
 
 import app.revanced.reddit.settings.SettingsEnum;
 
@@ -16,20 +11,21 @@ public class OpenLinksExternallyPatch {
     /**
      * Override 'CustomTabsIntent', in order to open links in the default browser.
      * Instead of doing CustomTabsActivity,
-     * this is done by building send data Intent from Android support library.
-     * <p>
-     * Modified implementation from developer.android.com/training/sharing/send
      *
      * @param activity The activity, to start an Intent.
      * @param uri      The URL to be opened in the default browser.
      */
-    public static void openLinksExternally(@NonNull Activity activity, @NonNull Uri uri) {
-        Intent createChooser = Intent.createChooser(new Intent("android.intent.action.VIEW", uri), null);
-        createChooser.putExtra("android.intent.extra.EXCLUDE_COMPONENTS", new Parcelable[]{new Intent(activity, RedditDeepLinkActivity.class)});
-        activity.startActivity(createChooser);
-    }
+    public static boolean openLinksExternally(Activity activity, Uri uri) {
+        if (activity == null || uri == null || !SettingsEnum.OPEN_LINKS_EXTERNALLY.getBoolean())
+            return false;
 
-    public static boolean openLinksExternally() {
-        return SettingsEnum.OPEN_LINKS_EXTERNALLY.getBoolean();
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(uri);
+            activity.startActivity(intent);
+            return true;
+        } catch (Exception ignored) {
+        }
+        return false;
     }
 }
--- dummy/src/main/java/com/reddit/frontpage/RedditDeepLinkActivity.java
+++
@@ -1,6 +0,0 @@
-package com.reddit.frontpage;
-
-import android.app.Activity;
-
-//Dummy class
-public class RedditDeepLinkActivity extends Activity { }
