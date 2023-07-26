7a60b691b523c8af501c8b73df0aa50ebd2b570a
inotia00 <ars258000@gmail.com>
2023-07-27 08:39:14 +0900

    build: bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0-alpha13")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
+        classpath("com.android.tools.build:gradle:8.2.0-alpha14")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
