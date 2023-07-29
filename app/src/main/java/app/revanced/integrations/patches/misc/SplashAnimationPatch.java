52e12d98133083ab60bb529f8921ff7599d76f72
inotia00 <ars258000@gmail.com>
2023-07-30 08:46:49 +0900

    feat(youtube): add support version `v18.29.38`

--- app/src/main/java/app/revanced/integrations/patches/misc/SplashAnimationPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/misc/SplashAnimationPatch.java
@@ -11,12 +11,28 @@ public class SplashAnimationPatch {
      * Context overrides when you open YouTube in an external browser.
      * This is expected to affect SplashAnimationPatch.
      */
-    public static boolean enableNewSplashAnimation() {
+    public static boolean enableNewSplashAnimationBoolean() {
         try {
             return getBoolean(REVANCED, "revanced_enable_new_splash_animation", false);
         } catch (Exception ex) {
             LogHelper.printException(SplashAnimationPatch.class, "Failed to load enableNewSplashAnimation", ex);
         }
         return false;
+    }
+
+    /**
+     * Context overrides when you open YouTube in an external browser.
+     * This is expected to affect SplashAnimationPatch.
+     */
+    public static int enableNewSplashAnimationInt() {
+        try {
+            return getBoolean(REVANCED, "revanced_enable_new_splash_animation", false)
+                    ? 3
+                    : 0;
+        } catch (Exception ex) {
+            LogHelper.printException(SplashAnimationPatch.class, "Failed to load enableNewSplashAnimation", ex);
+        }
+
+        return 0;
     }
 }
