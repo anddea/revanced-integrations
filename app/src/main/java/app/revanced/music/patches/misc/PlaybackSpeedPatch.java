c24bd815368fd2f5d9497c167c2508b7f98d92a0
inotia00 <ars258000@gmail.com>
2023-09-05 11:55:48 +0900

    feat(music): add `remember-playback-speed` patch

--- app/src/main/java/app/revanced/music/patches/misc/PlaybackSpeedPatch.java
+++ app/src/main/java/app/revanced/music/patches/misc/PlaybackSpeedPatch.java
@@ -1,5 +1,8 @@
 package app.revanced.music.patches.misc;
 
+import static app.revanced.music.utils.ReVancedUtils.showToastShort;
+import static app.revanced.music.utils.StringRef.str;
+
 import app.revanced.music.settings.SettingsEnum;
 
 public class PlaybackSpeedPatch {
@@ -19,5 +22,10 @@ public class PlaybackSpeedPatch {
 
     public static void userChangedSpeed(final float speed) {
         selectedSpeed = speed;
+
+        if (SettingsEnum.ENABLE_SAVE_PLAYBACK_SPEED.getBoolean()) {
+            SettingsEnum.DEFAULT_PLAYBACK_SPEED.saveValue(speed);
+            showToastShort(str("revanced_save_playback_speed") + "\u2009" + speed + "x");
+        }
     }
 }
--- app/src/main/java/app/revanced/music/settings/SettingsEnum.java
+++ app/src/main/java/app/revanced/music/settings/SettingsEnum.java
@@ -57,6 +57,7 @@ public enum SettingsEnum {
     // Misc
     ENABLE_DEBUG_LOGGING("revanced_enable_debug_logging", BOOLEAN, FALSE),
     ENABLE_OPUS_CODEC("revanced_enable_opus_codec", BOOLEAN, TRUE, true),
+    ENABLE_SAVE_PLAYBACK_SPEED("revanced_enable_save_playback_speed", BOOLEAN, FALSE),
     ENABLE_SAVE_VIDEO_QUALITY("revanced_enable_save_video_quality", BOOLEAN, TRUE),
     DEFAULT_PLAYBACK_SPEED("revanced_default_playback_speed", FLOAT, 1.0f),
     DEFAULT_VIDEO_QUALITY_WIFI("revanced_default_video_quality_wifi", INTEGER, -2),
