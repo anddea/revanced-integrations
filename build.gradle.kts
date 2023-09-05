b69ce25e651ead2aa7f8a08d35446f44be837822
inotia00 <ars258000@gmail.com>
2023-09-05 09:41:51 +0900

    build(integration): update gradle and dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0-alpha15")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        //noinspection AndroidGradlePluginVersion
+        classpath("com.android.tools.build:gradle:8.3.0-alpha01")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
