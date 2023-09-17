bc12e79b481e50ef3ae7bd07b65a20a9524ba854
inotia00 <ars258000@gmail.com>
2023-09-17 23:38:26 +0900

    fix(youtube/hide-shorts-components): no longer check navbar index when hiding the shorts header

--- app/src/main/java/app/revanced/integrations/patches/ads/ShortsFilter.java
+++ app/src/main/java/app/revanced/integrations/patches/ads/ShortsFilter.java
@@ -5,21 +5,14 @@ import androidx.annotation.Nullable;
 import app.revanced.integrations.settings.SettingsEnum;
 
 public final class ShortsFilter extends Filter {
+    private static final String REEL_CHANNEL_BAR_PATH = "reel_channel_bar.eml";
+
+    private final StringFilterGroup shortsShelfHeader;
 
     public ShortsFilter() {
         final var thanksButton = new StringFilterGroup(
                 SettingsEnum.HIDE_SHORTS_PLAYER_THANKS_BUTTON,
                 "suggested_action"
-        );
-
-        final var subscribeButton = new StringFilterGroup(
-                SettingsEnum.HIDE_SHORTS_PLAYER_SUBSCRIPTIONS_BUTTON,
-                "subscribe_button"
-        );
-
-        final var joinButton = new StringFilterGroup(
-                SettingsEnum.HIDE_SHORTS_PLAYER_JOIN_BUTTON,
-                "sponsor_button"
         );
 
         final var shorts = new StringFilterGroup(
@@ -30,17 +23,41 @@ public final class ShortsFilter extends Filter {
                 "shorts_video_cell"
         );
 
-        this.pathFilterGroups.addAll(joinButton, subscribeButton);
-        this.identifierFilterGroups.addAll(shorts, thanksButton);
+        shortsShelfHeader = new StringFilterGroup(
+                SettingsEnum.HIDE_SHORTS_SHELF,
+                "shelf_header.eml"
+        );
+
+        identifierFilterGroups.addAll(shorts, shortsShelfHeader, thanksButton);
+
+        final var joinButton = new StringFilterGroup(
+                SettingsEnum.HIDE_SHORTS_PLAYER_JOIN_BUTTON,
+                "sponsor_button"
+        );
+
+        final var subscribeButton = new StringFilterGroup(
+                SettingsEnum.HIDE_SHORTS_PLAYER_SUBSCRIPTIONS_BUTTON,
+                "subscribe_button"
+        );
+
+        pathFilterGroups.addAll(joinButton, subscribeButton);
     }
 
     @Override
     boolean isFiltered(String path, @Nullable String identifier, String allValue, byte[] protobufBufferArray,
                        FilterGroupList matchedList, FilterGroup matchedGroup, int matchedIndex) {
-        if (pathFilterGroups == matchedList) {
-            return path.contains("reel_channel_bar");
+        if (matchedList == pathFilterGroups) {
+            // Filter other path groups from pathFilterGroups, only when reelChannelBar is visible
+            // to avoid false positives.
+            if (!path.startsWith(REEL_CHANNEL_BAR_PATH))
+                return false;
+        } else if (matchedGroup == shortsShelfHeader) {
+            // Because the header is used in watch history and possibly other places, check for the index,
+            // which is 0 when the shelf header is used for Shorts.
+            if (matchedIndex != 0) return false;
         }
 
-        return identifierFilterGroups == matchedList;
+        // Super class handles logging.
+        return super.isFiltered(path, identifier, allValue, protobufBufferArray, matchedList, matchedGroup, matchedIndex);
     }
 }
