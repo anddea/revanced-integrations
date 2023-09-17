61ccc9612cc770befa0c69aa384f4e303d68395a
inotia00 <ars258000@gmail.com>
2023-09-17 23:46:10 +0900

    fix(youtube/navber-index-hook): no longer using litho filter

--- app/src/main/java/app/revanced/integrations/patches/utils/NavBarIndexPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/utils/NavBarIndexPatch.java
@@ -1,11 +1,8 @@
 package app.revanced.integrations.patches.utils;
 
-import android.content.Context;
-
 public class NavBarIndexPatch {
-    private static int currentNavBarIndex = 0;
-    private static int lastNavBarIndex = 0;
-    private static boolean isLaunched = false;
+    private static volatile int currentNavBarIndex = 0;
+    private static volatile int lastNavBarIndex = 0;
 
     public static void setCurrentNavBarIndex(int navBarIndex) {
         if (currentNavBarIndex == navBarIndex)
@@ -19,22 +16,8 @@ public class NavBarIndexPatch {
         currentNavBarIndex = lastNavBarIndex;
     }
 
-    public static boolean isNotLibraryTab() {
-        int collectionNavButtonIndex = 4;
-        return currentNavBarIndex < collectionNavButtonIndex;
-    }
-
     public static boolean isShortsTab() {
-        int collectionNavButtonIndex = 1;
-        return currentNavBarIndex == collectionNavButtonIndex;
-    }
-
-    public static void initializeIndex(Context context) {
-        if (isLaunched)
-            return;
-
-        isLaunched = true;
-        setCurrentNavBarIndex(0);
+        return currentNavBarIndex == 1;
     }
 }
 
