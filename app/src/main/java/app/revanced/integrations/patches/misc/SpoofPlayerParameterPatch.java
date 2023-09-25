13701edb28075c86c6a76743915a18be2522a549
inotia00 <ars258000@gmail.com>
2023-09-25 12:08:57 +0900

    feat(youtube/spoof-player-parameters): match to official ReVanced

--- app/src/main/java/app/revanced/integrations/patches/misc/SpoofPlayerParameterPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/misc/SpoofPlayerParameterPatch.java
@@ -4,8 +4,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import androidx.annotation.NonNull;
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,73 +14,58 @@ import app.revanced.integrations.utils.LogHelper;
 public class SpoofPlayerParameterPatch {
 
     /**
-     * Target player parameters.
+     * Parameter (also used by
+     * <a href="https://github.com/yt-dlp/yt-dlp/blob/81ca451480051d7ce1a31c017e005358345a9149/yt_dlp/extractor/youtube.py#L3602">yt-dlp</a>)
+     * to fix playback issues.
      */
-    private static final List<String> PLAYER_PARAMETER_WHITELIST = Arrays.asList(
+    private static final String INCOGNITO_PARAMETERS = "CgIQBg==";
+
+    /**
+     * Parameters causing playback issues.
+     */
+    private static final List<String> AUTOPLAY_PARAMETERS = Arrays.asList(
             "YAHI", // Autoplay in feed
             "SAFg"  // Autoplay in scrim
     );
 
     /**
-     * Player parameters parameters used in shorts.
+     * Parameters used in YouTube Shorts.
      */
-    private static final String PLAYER_PARAMETER_SHORTS = "8AEB";
-
-    /**
-     * Player parameters used in incognito mode's visitor data.
-     * <p>
-     * Known issue
-     * - Ambient mode may not work
-     * - Clip cannot be played normally
-     * - Downloading videos may not work
-     * - Filmstrip overlay are always hidden
-     * - No spoofing applied when watching previews in the feed
-     * - Seekbar thumbnails are hidden
-     */
-    private static final String PLAYER_PARAMETER_INCOGNITO = "CgIQBg==";
+    private static final String SHORTS_PLAYER_PARAMETERS = "8AEB";
 
     private static boolean isPlayingShorts;
 
     /**
      * Injection point.
      *
-     * @param playerParameter player parameter
+     * @param parameters Original player parameter value.
      */
-    public static String overridePlayerParameter(String playerParameter) {
-        try {
-            if (!SettingsEnum.SPOOF_PLAYER_PARAMETER.getBoolean()) {
-                return playerParameter;
-            }
+    public static String spoofParameter(String parameters) {
+        LogHelper.printDebug(SpoofPlayerParameterPatch.class, "Original player parameter value: " + parameters);
 
-            if (playerParameter.startsWith(PLAYER_PARAMETER_SHORTS)) {
-                isPlayingShorts = true;
-
-                return playerParameter;
-            }
-
-            isPlayingShorts = false;
-
-            LogHelper.printDebug(SpoofPlayerParameterPatch.class, "Original player parameter value: " + playerParameter);
-
-            boolean isPlayingFeed = PLAYER_PARAMETER_WHITELIST.stream().anyMatch(playerParameter::contains)
-                    && PlayerType.getCurrent() == PlayerType.INLINE_MINIMAL;
-
-            if (isPlayingFeed) {
-                // Spoofing is not applied when playing in the feed.
-                // It can prevent unintended histories from being added to your watching history.
-                // This will cause playback buffer issues for videos playing in the feed.
-                return playerParameter;
-            } else {
-                // Spoof the player parameter to prevent playback issues.
-                return PLAYER_PARAMETER_INCOGNITO;
-            }
-        } catch (Exception ex) {
-            LogHelper.printException(SpoofPlayerParameterPatch.class, "overrideProtobufParameter failure", ex);
+        if (!SettingsEnum.SPOOF_PLAYER_PARAMETER.getBoolean()) {
+            return parameters;
         }
 
-        return playerParameter;
-    }
+        // Shorts do not need to be spoofed.
+        // noinspection AssignmentUsedAsCondition
+        if (isPlayingShorts = parameters.startsWith(SHORTS_PLAYER_PARAMETERS)) {
+            return parameters;
+        }
 
+        final boolean isPlayingFeed = PlayerType.getCurrent() == PlayerType.INLINE_MINIMAL
+                && AUTOPLAY_PARAMETERS.stream().anyMatch(parameters::contains);
+
+        if (isPlayingFeed) {
+            // In order to prevent videos that are auto-played in feed to be added to history,
+            // only spoof the parameter if the video is not playing in the feed.
+            // This will cause playback issues in the feed, but it's better than manipulating the history.
+            return parameters;
+        } else {
+            // Spoof the player parameter to prevent playback issues.
+            return INCOGNITO_PARAMETERS;
+        }
+    }
 
     /**
      * Injection point.
@@ -96,18 +79,13 @@ public class SpoofPlayerParameterPatch {
      *
      * @param view seekbar thumbnail view.  Includes both shorts and regular videos.
      */
-    public static void seekbarImageViewCreated(@NonNull ImageView view) {
-        // seekbar thumbnail view does not need to be hidden in Shorts videos
-        if (!SettingsEnum.SPOOF_PLAYER_PARAMETER.getBoolean() || isPlayingShorts)
-            return;
+    public static void seekbarImageViewCreated(ImageView view) {
+        if (!SettingsEnum.SPOOF_PLAYER_PARAMETER.getBoolean()) return;
+        if (isPlayingShorts) return;
 
-        try {
-            view.setVisibility(View.GONE);
-            // Also hide the white border around the thumbnail (otherwise a 1 pixel wide bordered frame is visible).
-            ViewGroup parentLayout = (ViewGroup) view.getParent();
-            parentLayout.setPadding(0, 0, 0, 0);
-        } catch (Exception ex) {
-            LogHelper.printException(SpoofPlayerParameterPatch.class, "seekbarImageViewCreated failure", ex);
-        }
+        view.setVisibility(View.GONE);
+        // Also hide the border around the thumbnail (otherwise a 1 pixel wide bordered frame is visible).
+        ViewGroup parentLayout = (ViewGroup) view.getParent();
+        parentLayout.setPadding(0, 0, 0, 0);
     }
 }
