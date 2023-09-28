9dd205ef205f4be60e084391668f27da551f7a60
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-09-28 23:04:32 +0900

    fix(youtube/hide-flyout-panel): minor refactor

--- app/src/main/java/app/revanced/integrations/patches/ads/PlayerFlyoutPanelsFooterFilter.java
+++ app/src/main/java/app/revanced/integrations/patches/ads/PlayerFlyoutPanelsFooterFilter.java
@@ -3,15 +3,27 @@ package app.revanced.integrations.patches.ads;
 import androidx.annotation.Nullable;
 
 import app.revanced.integrations.settings.SettingsEnum;
+import app.revanced.integrations.utils.StringTrieSearch;
 
 final class PlayerFlyoutPanelsFooterFilter extends Filter {
 
+    private final StringTrieSearch exceptions = new StringTrieSearch();
+    private final StringTrieSearch targetPath = new StringTrieSearch();
+
     public PlayerFlyoutPanelsFooterFilter() {
+        exceptions.addPattern(
+                "bottom_sheet_list_option"
+        );
+
+        targetPath.addPatterns(
+                "captions_sheet_content.eml",
+                "quality_sheet_content.eml"
+        );
 
         pathFilterGroups.addAll(
             new StringFilterGroup(
                 SettingsEnum.HIDE_PLAYER_FLYOUT_PANEL_QUALITY_FOOTER,
-                "quality_sheet_footer",
+                "quality_sheet_footer.eml",
                 "|divider.eml|"
             ),
             new StringFilterGroup(
@@ -25,10 +37,9 @@ final class PlayerFlyoutPanelsFooterFilter extends Filter {
     @Override
     boolean isFiltered(String path, @Nullable String identifier, String allValue, byte[] protobufBufferArray,
                        FilterGroupList matchedList, FilterGroup matchedGroup, int matchedIndex) {
-        // If the flyout is not caption or quality, skip
-        if ( (path.contains("captions_sheet_content.eml-js") || path.contains("advanced_quality_sheet_content.eml-js")) && !path.contains("bottom_sheet_list_option"))
-            return super.isFiltered(path, identifier, allValue, protobufBufferArray, matchedList, matchedGroup, matchedIndex);
+        if (exceptions.matches(path) || !targetPath.matches(path))
+            return false;
 
-        return false;
+        return super.isFiltered(path, identifier, allValue, protobufBufferArray, matchedList, matchedGroup, matchedIndex);
     }
 }
