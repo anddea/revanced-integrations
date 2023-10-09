bec9b4cd3af3f2f7c5ee10dda67fe28f640cf293
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-10-09 23:10:52 +0900

    build: bump patcher to `16.0.2`

--- app/src/main/java/app/revanced/integrations/patches/misc/SpoofAppVersionPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/misc/SpoofAppVersionPatch.java
@@ -6,20 +6,20 @@ import static app.revanced.integrations.utils.SharedPrefHelper.getString;
 
 import app.revanced.integrations.utils.LogHelper;
 
-public class VersionOverridePatch {
+public class SpoofAppVersionPatch {
 
     /**
      * Context is overridden when trying to play a YouTube video from the Google Play Store,
-     * Which is speculated to affect VersionOverridePatch
+     * Which is speculated to affect SpoofAppVersionPatch
      */
     public static String getVersionOverride(String version) {
 
         try {
             return getBoolean(REVANCED, "revanced_spoof_app_version", false)
-                    ? getString(REVANCED, "revanced_spoof_app_version_target", "18.20.39")
+                    ? getString(REVANCED, "revanced_spoof_app_version_target", "18.17.43")
                     : version;
         } catch (Exception ex) {
-            LogHelper.printException(VersionOverridePatch.class, "Failed to load getVersionOverride", ex);
+            LogHelper.printException(SpoofAppVersionPatch.class, "Failed to load getVersionOverride", ex);
             return version;
         }
     }
