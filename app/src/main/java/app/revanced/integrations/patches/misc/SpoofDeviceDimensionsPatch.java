ae87dcb0817465727a5b28d2e4246dc58a5a3876
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-10-21 10:48:36 +0900

    feat(YouTube): add `Spoof device dimensions` patch

---
+++ app/src/main/java/app/revanced/integrations/patches/misc/SpoofDeviceDimensionsPatch.java
@@ -0,0 +1,15 @@
+package app.revanced.integrations.patches.misc;
+
+import app.revanced.integrations.settings.SettingsEnum;
+
+public class SpoofDeviceDimensionsPatch {
+    private static final boolean SPOOF = SettingsEnum.SPOOF_DEVICE_DIMENSIONS.getBoolean();
+
+    public static int getMinHeightOrWidth(int minHeightOrWidth) {
+        return SPOOF ? 64 : minHeightOrWidth;
+    }
+
+    public static int getMaxHeightOrWidth(int maxHeightOrWidth) {
+        return SPOOF ? 4096 : maxHeightOrWidth;
+    }
+}
