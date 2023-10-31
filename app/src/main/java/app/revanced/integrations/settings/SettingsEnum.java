8b28eea8a58de088c2ad2d20171a533bdaf82679
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-10-31 20:05:52 +0900

    fix(YouTube/Enable new splash animation): change default value

--- app/src/main/java/app/revanced/integrations/settings/SettingsEnum.java
+++ app/src/main/java/app/revanced/integrations/settings/SettingsEnum.java
@@ -192,7 +192,7 @@ public enum SettingsEnum {
     ENABLE_DEBUG_BUFFER_LOGGING("revanced_enable_debug_buffer_logging", BOOLEAN, FALSE),
     ENABLE_EXTERNAL_BROWSER("revanced_enable_external_browser", BOOLEAN, TRUE, true),
     ENABLE_LANGUAGE_SWITCH("revanced_enable_language_switch", BOOLEAN, TRUE, true),
-    ENABLE_NEW_SPLASH_ANIMATION("revanced_enable_new_splash_animation", BOOLEAN, FALSE, true),
+    ENABLE_NEW_SPLASH_ANIMATION("revanced_enable_new_splash_animation", BOOLEAN, TRUE, true),
     ENABLE_OPEN_LINKS_DIRECTLY("revanced_enable_open_links_directly", BOOLEAN, TRUE),
     DOUBLE_BACK_TIMEOUT("revanced_double_back_timeout", INTEGER, 2, true),
 
