f5909d673dc154e86afe4638ff504e12ee74c96f
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-11-06 17:10:33 +0900

    build: bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ buildscript {
     }
     dependencies {
         //noinspection AndroidGradlePluginVersion
-        classpath("com.android.tools.build:gradle:8.3.0-alpha11")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+        classpath("com.android.tools.build:gradle:8.3.0-alpha12")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
