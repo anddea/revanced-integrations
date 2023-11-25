0c682641df643db0d70c431336acb0d9eb8f6625
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-11-25 19:37:35 +0900

    chore: merge official Revanced's change

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ReVanced Integrations  
- 
+# 🔩 ReVanced Integrations  
+
+ReVanced Extended Integrations containing classes to be merged by ReVanced Patcher.
+
+## ❓ How to use debugging:
+
+- Usage on Windows: ```adb logcat | findstr "Extended" > log.txt```
+- Usage on Linux: ```adb logcat | grep --line-buffered "Extended" > log.txt```
+  
+This will write the log to a file called log.txt which you can view then.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
 @file:Suppress("DEPRECATION")
-
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
         google()
@@ -10,9 +8,14 @@ buildscript {
         //noinspection AndroidGradlePluginVersion
         classpath("com.android.tools.build:gradle:8.0.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    }
+}
 
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+allprojects {
+    repositories {
+        google()
+        maven { url = uri("https://jitpack.io") }
+        mavenCentral()
     }
 }
 
