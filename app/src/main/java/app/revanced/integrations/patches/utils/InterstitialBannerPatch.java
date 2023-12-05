47386931626fcdce920b02c4df047d09a5f4d486
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-12-06 03:24:25 +0900

    feat(YouTube/Hide general ads): toast message is shown when closing `interstitial ads`

--- app/src/main/java/app/revanced/integrations/patches/utils/InterstitialBannerPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/utils/InterstitialBannerPatch.java
@@ -1,12 +1,10 @@
 package app.revanced.integrations.patches.utils;
 
+import static app.revanced.integrations.utils.StringRef.str;
+
 import android.app.Instrumentation;
-import android.icu.text.DateFormat;
 import android.view.KeyEvent;
 
-import java.util.Date;
-
-import app.revanced.integrations.utils.LogHelper;
 import app.revanced.integrations.utils.ReVancedUtils;
 
 public class InterstitialBannerPatch {
@@ -21,7 +19,7 @@ public class InterstitialBannerPatch {
             return;
 
         lastTimeCalled = currentTime;
-        LogHelper.printDebug(() -> "onBackPressed: " + DateFormat.getDateTimeInstance().format(new Date(lastTimeCalled)));
         ReVancedUtils.runOnMainThreadDelayed(() -> new Thread(() -> new Instrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_BACK)).start(), 1000);
+        ReVancedUtils.runOnMainThreadDelayed(() -> ReVancedUtils.showToastShort(str("revanced_close_interstitial_ads_toast")), 1000);
     }
 }
