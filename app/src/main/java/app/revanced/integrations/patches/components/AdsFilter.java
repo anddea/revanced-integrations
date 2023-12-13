7fe8886fe4e4f7b2473f3f5418ea3ac3963b8ad9
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-12-13 19:39:40 +0900

    fix(YouTube/Hide general ads): new types of ads are not blocked

--- app/src/main/java/app/revanced/integrations/patches/components/AdsFilter.java
+++ app/src/main/java/app/revanced/integrations/patches/components/AdsFilter.java
@@ -2,30 +2,13 @@ package app.revanced.integrations.patches.components;
 
 import android.view.View;
 
-import androidx.annotation.Nullable;
-
 import app.revanced.integrations.settings.SettingsEnum;
 import app.revanced.integrations.utils.ReVancedUtils;
-import app.revanced.integrations.utils.StringTrieSearch;
 
-/**
- * @noinspection rawtypes
- */
 @SuppressWarnings("unused")
 public final class AdsFilter extends Filter {
-    private final StringTrieSearch exceptions = new StringTrieSearch();
 
     public AdsFilter() {
-        exceptions.addPatterns(
-                "download_",
-                "downloads_",
-                "home_video_with_context", // Don't filter anything in the home page video component.
-                "library_recent_shelf",
-                "playlist_add",
-                "related_video_with_context", // Don't filter anything in the related video component.
-                "video_description_header",
-                "|comment." // skip blocking anything in the comments replies
-        );
 
         final StringFilterGroup carouselAd = new StringFilterGroup(
                 SettingsEnum.HIDE_GENERAL_ADS,
@@ -39,7 +22,6 @@ public final class AdsFilter extends Filter {
 
         final StringFilterGroup merchandise = new StringFilterGroup(
                 SettingsEnum.HIDE_MERCHANDISE_SHELF,
-                "expandable_list_inner",
                 "product_carousel"
         );
 
@@ -55,16 +37,13 @@ public final class AdsFilter extends Filter {
 
         final StringFilterGroup generalAds = new StringFilterGroup(
                 SettingsEnum.HIDE_GENERAL_ADS,
-                "active_view_display_container",
-                "ads_",
                 "ads_video_with_context",
-                "ad_",
                 "banner_text_icon",
-                "brand_video_shelf",
-                "brand_video_singleton",
+                "brand_video",
                 "button_banner",
                 "carousel_footered_layout",
                 "carousel_headered_layout",
+                "full_width_portrait_image_layout",
                 "full_width_square_image_layout",
                 "hero_promo_image",
                 "landscape_image_wide_button_layout",
@@ -77,15 +56,12 @@ public final class AdsFilter extends Filter {
                 "text_image_button_group_layout",
                 "text_image_button_layout",
                 "video_display_button_group_layout",
-                "video_display_carousel_buttoned_layout",
+                "video_display_carousel_button",
                 "video_display_full_layout",
                 "watch_metadata_app_promo",
-                "_ad",
-                "_ads",
                 "_ad_with",
                 "_buttoned_layout",
-                "|ads_",
-                "|ad_"
+                "_image_layout"
         );
 
         final StringFilterGroup viewProducts = new StringFilterGroup(
@@ -124,14 +100,5 @@ public final class AdsFilter extends Filter {
 
     public static boolean hideGetPremium() {
         return SettingsEnum.HIDE_GET_PREMIUM.getBoolean();
-    }
-
-    @Override
-    boolean isFiltered(String path, @Nullable String identifier, String allValue, byte[] protobufBufferArray,
-                       FilterGroupList matchedList, FilterGroup matchedGroup, int matchedIndex) {
-        if (exceptions.matches(path))
-            return false;
-
-        return super.isFiltered(path, identifier, allValue, protobufBufferArray, matchedList, matchedGroup, matchedIndex);
     }
 }
