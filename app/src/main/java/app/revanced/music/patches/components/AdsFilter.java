05d365f21e800a7dbaff5c22c25d667fd6de3924
inotia00 <108592928+inotia00@users.noreply.github.com>
2023-12-23 16:36:23 +0900

    feat(YouTube Music/Hide general ads): remove `Close interstitial ads` settings

--- app/src/main/java/app/revanced/music/patches/components/AdsFilter.java
+++ app/src/main/java/app/revanced/music/patches/components/AdsFilter.java
@@ -1,43 +1,16 @@
 package app.revanced.music.patches.components;
 
-import app.revanced.music.patches.utils.InterstitialBannerPatch;
 import app.revanced.music.settings.SettingsEnum;
 
-/**
- * @noinspection rawtypes
- */
 @SuppressWarnings("unused")
 public final class AdsFilter extends Filter {
-    private final StringFilterGroup interstitialBanner;
-    private final StringFilterGroup statementBanner;
 
     public AdsFilter() {
-        interstitialBanner = new StringFilterGroup(
-                SettingsEnum.CLOSE_INTERSTITIAL_ADS,
-                "_interstitial"
-        );
-
-        statementBanner = new StringFilterGroup(
+        final StringFilterGroup statementBanner = new StringFilterGroup(
                 SettingsEnum.HIDE_GENERAL_ADS,
                 "statement_banner"
         );
 
-        pathFilterGroupList.addAll(interstitialBanner, statementBanner);
-    }
-
-    @Override
-    boolean isFiltered(String path, FilterGroupList matchedList, FilterGroup matchedGroup, int matchedIndex) {
-        if (matchedGroup == statementBanner) {
-            return super.isFiltered(path, matchedList, matchedGroup, matchedIndex);
-        } else if (matchedGroup == interstitialBanner && path.contains("|ImageType|")) {
-            // If you hide the entire banner, the layout is not loaded,
-            // So only the empty gray screen is displayed.
-            // https://github.com/ReVanced/revanced-integrations/pull/355
-
-            // Therefore, instead of hiding the entire banner,
-            // If the banner is detected, just press the back button.
-            InterstitialBannerPatch.onBackPressed();
-        }
-        return false;
+        pathFilterGroupList.addAll(statementBanner);
     }
 }
