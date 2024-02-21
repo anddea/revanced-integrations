e0a98abd67fe0203edd3124df10a4e9edfb21483
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2024-02-21 17:19:41 +0700

    Update VideoHelpers.java

--- app/src/main/java/app/revanced/integrations/youtube/utils/VideoHelpers.java
+++ app/src/main/java/app/revanced/integrations/youtube/utils/VideoHelpers.java
@@ -132,6 +132,11 @@ public class VideoHelpers {
     }
 
     public static void playlistFromChannelVideosListener(@NonNull Context context, boolean activated) {
+        // This patches require Open links directly to be worked
+        if (!SettingsEnum.ENABLE_OPEN_LINKS_DIRECTLY.getBoolean()) {
+            SettingsEnum.ENABLE_OPEN_LINKS_DIRECTLY.saveBoolean(true);
+            ReVancedUtils.runOnMainThreadDelayed(() -> SettingsEnum.ENABLE_OPEN_LINKS_DIRECTLY.saveBoolean(false), 1000L);
+        }
         String baseUri = "https://youtu.be/" + VideoInformation.getVideoId() + "?t=" + VideoInformation.getVideoTime() / 1000;
         if (activated) {
             baseUri += "&list=UL" + VideoInformation.getVideoId();
