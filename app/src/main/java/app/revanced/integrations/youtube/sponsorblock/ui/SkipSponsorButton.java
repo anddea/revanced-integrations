9ce00f6d4599baad9008559d1238bea8e0610242
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2024-02-24 12:54:23 +0700

    fix(YouTube/Sponsorblock): Modernize skip buttons

--- app/src/main/java/app/revanced/integrations/youtube/sponsorblock/ui/SkipSponsorButton.java
+++ app/src/main/java/app/revanced/integrations/youtube/sponsorblock/ui/SkipSponsorButton.java
@@ -4,8 +4,6 @@ import static app.revanced.integrations.youtube.utils.ResourceUtils.identifier;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,14 +20,10 @@ import app.revanced.integrations.youtube.sponsorblock.objects.SponsorSegment;
 import app.revanced.integrations.youtube.utils.ResourceType;
 
 public class SkipSponsorButton extends FrameLayout {
-    private static final boolean highContrast = true;
     final int defaultBottomMargin;
     final int ctaBottomMargin;
     final int hiddenBottomMargin;
-    private final LinearLayout skipSponsorBtnContainer;
     private final TextView skipSponsorTextView;
-    private final Paint background;
-    private final Paint border;
     private SponsorSegment segment;
 
     public SkipSponsorButton(Context context) {
@@ -49,14 +43,7 @@ public class SkipSponsorButton extends FrameLayout {
 
         LayoutInflater.from(context).inflate(identifier("skip_sponsor_button", ResourceType.LAYOUT, context), this, true);  // layout:skip_ad_button
         setMinimumHeight(getResources().getDimensionPixelSize(identifier("ad_skip_ad_button_min_height", ResourceType.DIMEN, context)));  // dimen:ad_skip_ad_button_min_height
-        skipSponsorBtnContainer = (LinearLayout) Objects.requireNonNull((View) findViewById(identifier("sb_skip_sponsor_button_container", ResourceType.ID, context)));  // id:skip_ad_button_container
-        background = new Paint();
-        background.setColor(context.getColor(identifier("skip_ad_button_background_color", ResourceType.COLOR, context)));  // color:skip_ad_button_background_color);
-        background.setStyle(Paint.Style.FILL);
-        border = new Paint();
-        border.setColor(context.getColor(identifier("skip_ad_button_border_color", ResourceType.COLOR, context)));  // color:skip_ad_button_border_color);
-        border.setStrokeWidth(getResources().getDimension(identifier("ad_skip_ad_button_border_width", ResourceType.DIMEN, context)));  // dimen:ad_skip_ad_button_border_width
-        border.setStyle(Paint.Style.STROKE);
+        LinearLayout skipSponsorBtnContainer = (LinearLayout) Objects.requireNonNull((View) findViewById(identifier("sb_skip_sponsor_button_container", ResourceType.ID, context)));  // id:skip_ad_button_container
         skipSponsorTextView = (TextView) Objects.requireNonNull((View) findViewById(identifier("sb_skip_sponsor_button_text", ResourceType.ID, context)));  // id:sb_skip_sponsor_button_text;
         Resources resources = context.getResources();
         defaultBottomMargin = resources.getDimensionPixelSize(identifier("skip_button_default_bottom_margin", ResourceType.DIMEN, context));  // dimen:skip_button_default_bottom_margin
@@ -68,24 +55,6 @@ public class SkipSponsorButton extends FrameLayout {
             setVisibility(View.GONE);
             SegmentPlaybackController.onSkipSegmentClicked(segment);
         });
-    }
-
-    @Override  // android.view.ViewGroup
-    protected final void dispatchDraw(Canvas canvas) {
-        final int left = skipSponsorBtnContainer.getLeft();
-        final int top = skipSponsorBtnContainer.getTop();
-        final int leftPlusWidth = (left + skipSponsorBtnContainer.getWidth());
-        final int topPlusHeight = (top + skipSponsorBtnContainer.getHeight());
-        canvas.drawRect(left, top, leftPlusWidth, topPlusHeight, background);
-        if (!highContrast) {
-            canvas.drawLines(new float[]{
-                            leftPlusWidth, top, left, top,
-                            left, top, left, topPlusHeight,
-                            left, topPlusHeight, leftPlusWidth, topPlusHeight},
-                    border);
-        }
-
-        super.dispatchDraw(canvas);
     }
 
     /**
