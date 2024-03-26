89d9b17e3f163a7c0f2c9461dda65c808db4b07c
Aaron Veil <70171475+anddea@users.noreply.github.com>
2024-03-26 17:01:51 +0300

    fix(YouTube - Hide interstitial ads): Prevent app crash if hiding interstitial ads is not possible

--- app/src/main/java/app/revanced/integrations/youtube/patches/utils/InterstitialBannerPatch.java
+++ app/src/main/java/app/revanced/integrations/youtube/patches/utils/InterstitialBannerPatch.java
@@ -5,6 +5,7 @@ import static app.revanced.integrations.youtube.utils.StringRef.str;
 import android.app.Instrumentation;
 import android.view.KeyEvent;
 
+import app.revanced.integrations.youtube.settings.SettingsEnum;
 import app.revanced.integrations.youtube.utils.ReVancedUtils;
 
 public class InterstitialBannerPatch {
@@ -21,8 +22,22 @@ public class InterstitialBannerPatch {
 
         lastTimeCalled = currentTime;
         ReVancedUtils.runOnMainThreadDelayed(() -> {
-            // Must run off main thread (Odd, but whatever).
-            ReVancedUtils.runOnBackgroundThread(() -> instrumentation.sendKeyDownUpSync(KeyEvent.KEYCODE_BACK));
+            ReVancedUtils.runOnBackgroundThread(() -> {
+                try {
+                    instrumentation.sendKeyDownUpSync(KeyEvent.KEYCODE_BACK);
+                } catch (Exception ex) {
+                    // Injecting user events on Android 10+ requires the manifest to include
+                    // INJECT_EVENTS, and it's usage is heavily restricted
+                    // and requires the user to manually approve the permission in the device settings.
+                    //
+                    // And no matter what, permissions cannot be added for root installations
+                    // as manifest changes are ignored for mount installations.
+                    //
+                    // Instead, catch the SecurityException and turn off hide full screen ads
+                    // since this functionality does not work for these devices.
+                    SettingsEnum.CLOSE_INTERSTITIAL_ADS.saveValue(false);
+                }
+            });
         }, 1000);
         ReVancedUtils.runOnMainThreadDelayed(() -> ReVancedUtils.showToastShort(str("revanced_close_interstitial_ads_toast")), 1000);
     }
