67f84febaf51dc50c0cfe39abfcf88a3d2f2d2bb
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-06-29 00:32:19 +0900

    fix(YouTube - Hide feed components): Video filters do not work properly on accounts with A/B testing applied

--- app/src/main/java/app/revanced/integrations/youtube/patches/components/FeedVideoFilter.java
+++ app/src/main/java/app/revanced/integrations/youtube/patches/components/FeedVideoFilter.java
@@ -3,8 +3,10 @@ package app.revanced.integrations.youtube.patches.components;
 import androidx.annotation.Nullable;
 
 import app.revanced.integrations.shared.patches.components.ByteArrayFilterGroup;
+import app.revanced.integrations.shared.patches.components.ByteArrayFilterGroupList;
 import app.revanced.integrations.shared.patches.components.Filter;
 import app.revanced.integrations.shared.patches.components.StringFilterGroup;
+import app.revanced.integrations.shared.utils.StringTrieSearch;
 import app.revanced.integrations.youtube.settings.Settings;
 import app.revanced.integrations.youtube.shared.RootView;
 
@@ -12,45 +14,61 @@ import app.revanced.integrations.youtube.shared.RootView;
  * @noinspection ALL
  */
 public final class FeedVideoFilter extends Filter {
-    private final ByteArrayFilterGroup feedRecommendations = new ByteArrayFilterGroup(
-            null,
-            "endorsement_header_footer" // videos with gray descriptions
-    );
-    private final ByteArrayFilterGroup videosForMembershipOnly = new ByteArrayFilterGroup(
-            Settings.HIDE_RECOMMENDED_VIDEO,
-            "high-ptsZ" // videos for membership only
-    );
-    private final ByteArrayFilterGroup videosWithLowView = new ByteArrayFilterGroup(
-            Settings.HIDE_LOW_VIEWS_VIDEO,
-            "g-highZ"  // videos with less than 1000 views
-    );
+    private static final String CONVERSATION_CONTEXT_FEED_IDENTIFIER =
+            "horizontalCollectionSwipeProtector=null";
+
+    private static final StringTrieSearch feedVideoPattern = new StringTrieSearch();
+
     // In search results, vertical video with shorts labels mostly include videos with gray descriptions.
     // Filters without check process.
     private final StringFilterGroup inlineShorts;
-    private final StringFilterGroup feedVideo;
-    private final StringFilterGroup homeFeedVideoFilter;
-
+    private final StringFilterGroup videoLockup;
+    private final ByteArrayFilterGroupList bufferGroupList = new ByteArrayFilterGroupList();
 
     public FeedVideoFilter() {
-        homeFeedVideoFilter = new StringFilterGroup(
+        feedVideoPattern.addPattern(CONVERSATION_CONTEXT_FEED_IDENTIFIER);
+
+        final StringFilterGroup homeFeedVideo = new StringFilterGroup(
                 null,
-                "home_video_with_context.eml",
-                "video_lockup_with_attachment.eml"
+                "home_video_with_context.eml"
         );
-        feedVideo = new StringFilterGroup(
+        final StringFilterGroup feedVideo = new StringFilterGroup(
                 Settings.HIDE_RECOMMENDED_VIDEO,
-                "video_with_context.eml"
+                "related_video_with_context.eml",
+                "search_video_with_context.eml"
         );
         inlineShorts = new StringFilterGroup(
                 Settings.HIDE_RECOMMENDED_VIDEO,
                 "inline_shorts.eml" // vertical video with shorts label
         );
 
-        // Paths.
-        addPathCallbacks(
-                inlineShorts,
-                homeFeedVideoFilter,
-                feedVideo
+        addIdentifierCallbacks(
+                homeFeedVideo,
+                feedVideo,
+                inlineShorts
+        );
+
+        // Used for home, related videos, subscriptions, and search results.
+        videoLockup = new StringFilterGroup(
+                null,
+                    "video_lockup_with_attachment.eml"
+        );
+
+        addPathCallbacks(videoLockup);
+
+        bufferGroupList.addAll(
+                new ByteArrayFilterGroup(
+                        Settings.HIDE_RECOMMENDED_VIDEO,
+                        "endorsement_header_footer" // videos with gray descriptions
+                ),
+                new ByteArrayFilterGroup(
+                        Settings.HIDE_RECOMMENDED_VIDEO,
+                        "high-ptsZ" // videos for membership only
+                ),
+                new ByteArrayFilterGroup(
+                        Settings.HIDE_LOW_VIEWS_VIDEO,
+                        "g-highZ"  // videos with less than 1000 views
+                )
         );
     }
 
@@ -58,20 +76,15 @@ public final class FeedVideoFilter extends Filter {
     public boolean isFiltered(String path, @Nullable String identifier, String allValue, byte[] protobufBufferArray,
                               StringFilterGroup matchedGroup, FilterContentType contentType, int contentIndex) {
         if (matchedGroup == inlineShorts) {
-            if (contentIndex != 0 || !RootView.isSearchBarActive()) {
+            if (!RootView.isSearchBarActive()) {
                 return false;
             }
         } else {
-            if (matchedGroup == feedVideo && !feedRecommendations.check(protobufBufferArray).isFiltered()) {
+            if (matchedGroup == videoLockup && !feedVideoPattern.matches(allValue)) {
                 return false;
             }
-            if (matchedGroup == homeFeedVideoFilter) {
-                if (contentIndex != 0) {
-                    return false;
-                }
-                if (!videosForMembershipOnly.check(protobufBufferArray).isFiltered() && !videosWithLowView.check(protobufBufferArray).isFiltered()) {
-                    return false;
-                }
+            if (!bufferGroupList.check(protobufBufferArray).isFiltered()) {
+                return false;
             }
         }
 
