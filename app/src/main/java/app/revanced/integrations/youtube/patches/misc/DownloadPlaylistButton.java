c74cbbcb167d8d0a7e63df67d9a5f7600d2e85d3
Francesco146 <marastoni14@gmail.com>
2024-07-29 11:56:29 +0200

    fix(YouTube - Download Playlist Button): Make the button fully visible and refactor

--- app/src/main/java/app/revanced/integrations/youtube/patches/misc/DownloadPlaylistButton.java
+++ app/src/main/java/app/revanced/integrations/youtube/patches/misc/DownloadPlaylistButton.java
@@ -1,32 +1,11 @@
 package app.revanced.integrations.youtube.patches.misc;
 
-import android.graphics.Color;
-import android.view.View;
-import app.revanced.integrations.shared.utils.Logger;
-
 @SuppressWarnings("unused")
 public class DownloadPlaylistButton {
-
     /*
      * Injection point
      */
-    public static int enablePlaylistDownloadButton(int i) {
-        Logger.printInfo(() ->
-                "[DownloadPlaylistButton] Enabling playlist download button, " +
-                        "before: " + i
-        );
-        return 2;
-    }
-
-    /*
-     * Injection point
-     */
-    public static void setPlaylistDownloadButtonVisibility(View offlineArrowView) {
-        Logger.printInfo(() ->
-                "[DownloadPlaylistButton] Setting playlist download button visibility, " +
-                        "before: " + (offlineArrowView == null ? "null" : offlineArrowView.toString())
-        );
-        offlineArrowView.setVisibility(View.VISIBLE);
-        offlineArrowView.setEnabled(true);
+    public static boolean setPlaylistDownloadButtonVisibility() {
+        return true;
     }
 }
