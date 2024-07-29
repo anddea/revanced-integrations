1cb063b1bb0b0ba1b7db97683deb32e9325db91f
Francesco146 <marastoni14@gmail.com>
2024-07-29 16:16:54 +0200

    feat(YouTube - Download Playlist Button): Hook playlist download button onClick method

--- app/src/main/java/app/revanced/integrations/youtube/patches/misc/DownloadPlaylistButton.java
+++ app/src/main/java/app/revanced/integrations/youtube/patches/misc/DownloadPlaylistButton.java
@@ -1,11 +1,28 @@
 package app.revanced.integrations.youtube.patches.misc;
 
+import app.revanced.integrations.shared.utils.Logger;
+
 @SuppressWarnings("unused")
 public class DownloadPlaylistButton {
     /*
      * Injection point
      */
-    public static boolean setPlaylistDownloadButtonVisibility() {
+    public static boolean isPlaylistDownloadButtonHooked() {
         return true;
+    }
+
+    /*
+     * Injection point
+     */
+    public static String startPlaylistDownloadActivity(String playlistId) {
+        // Hook the playlist download button
+        Logger.printInfo(() -> "[DownloadPlaylistButton] String: " + playlistId);
+
+        if (!isPlaylistDownloadButtonHooked()) return playlistId;
+
+
+        // return an empty string to prevent
+        // the original method from being called
+        return "";
     }
 }
