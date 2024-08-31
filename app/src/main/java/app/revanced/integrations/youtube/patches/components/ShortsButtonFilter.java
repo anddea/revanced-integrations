2a8cb5a1c520c4ff9d98fd44a322fec073ba448a
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-09-01 02:25:00 +0900

    feat(YouTube - Shorts components): Add `Hide Use this sound button` setting

--- app/src/main/java/app/revanced/integrations/youtube/patches/components/ShortsButtonFilter.java
+++ app/src/main/java/app/revanced/integrations/youtube/patches/components/ShortsButtonFilter.java
@@ -174,6 +174,10 @@ public final class ShortsButtonFilter extends Filter {
                 new ByteArrayFilterGroup(
                         Settings.HIDE_SHORTS_SUPER_THANKS_BUTTON,
                         "yt_outline_dollar_sign_heart_"
+                ),
+                new ByteArrayFilterGroup(
+                        Settings.HIDE_SHORTS_USE_THIS_SOUND_BUTTON,
+                        "yt_outline_camera"
                 )
         );
     }
