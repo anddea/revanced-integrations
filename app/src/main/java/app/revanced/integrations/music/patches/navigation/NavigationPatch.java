6a05e8f90627d054734c692e5eae8d2843fea50c
Aaron Veil <70171475+anddea@users.noreply.github.com>
2024-10-23 18:01:20 +0300

    feat(YouTube Music - Navigation bar components): Do not use hardcoded color, `Enable black navigation bar` setting is turned off

--- app/src/main/java/app/revanced/integrations/music/patches/navigation/NavigationPatch.java
+++ app/src/main/java/app/revanced/integrations/music/patches/navigation/NavigationPatch.java
@@ -2,19 +2,25 @@ package app.revanced.integrations.music.patches.navigation;
 
 import static app.revanced.integrations.shared.utils.Utils.hideViewUnderCondition;
 
+import android.graphics.Color;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
 import app.revanced.integrations.music.settings.Settings;
+import app.revanced.integrations.shared.utils.ResourceUtils;
 
 @SuppressWarnings("unused")
 public class NavigationPatch {
+    private static final int colorGrey12 =
+            ResourceUtils.getColor("revanced_color_grey_12");
     public static Enum<?> lastPivotTab;
 
     public static int enableBlackNavigationBar() {
-        return Settings.ENABLE_BLACK_NAVIGATION_BAR.get() ? -16777216 : -14869219;
+        return Settings.ENABLE_BLACK_NAVIGATION_BAR.get()
+                ? Color.BLACK
+                : colorGrey12;
     }
 
     public static void hideNavigationLabel(TextView textview) {
